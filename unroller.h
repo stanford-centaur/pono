a3c398e0f88e60b8b13c18b61f4165a962d6ea3e
Ahmed Irfan <irfan@cs.stanford.edu>
2019-09-06 10:39:31 -0700

    unroller header file

---
+++ unroller.h
@@ -0,0 +1,38 @@
+#ifndef UNROLLER_H
+#define UNROLLER_H
+
+#include "rts.h"
+
+#include "smt.h"
+
+namespace cosa
+{
+  
+  /* 
+   * unroller inspired by the ic3ia unroller:
+   * https://es-static.fbk.eu/people/griggio/ic3ia/
+   *
+   */
+  
+class Unroller
+{
+ public:
+  Unroller(const RelationalTransitionSystem &ts);
+
+  smt::term at_time(smt::term t, unsigned int k);
+
+  smt::untime untime(smt::term t);
+
+ private:
+
+  const RelationalTransitionSystem &ts_;
+
+  typedef std::vector<UnorderedTermMap *> TimeCache;
+  TimeCache time_cache_;
+  UnorderedTermMap untime_cache_;
+  
+}; // class Unroller
+
+} // namespace cosa
+
+#endif
