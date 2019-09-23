ff11bcd1f121ebac6ad5d7ee25f7cbb910eb50ae
Makai Mann <makaim@stanford.edu>
2019-09-23 16:05:47 -0700

    Add cmake build system

---
+++ configure.sh
@@ -0,0 +1,81 @@
+#!/bin/sh
+
+# Syntax and structure borrowed from CVC4's configure.sh script
+
+CONF_FILE=Makefile.conf
+
+usage () {
+cat <<EOF
+Usage: $0 [<option> ...]
+
+Configures the CMAKE build environment.
+
+-h, --help              display this message and exit
+--prefix=STR            install directory       (default: /usr/local/)
+--build-dir=STR         custom build directory  (default: build)
+--debug                 build debug with debug symbols (default: off)
+EOF
+  exit 0
+}
+
+die () {
+    echo "*** $0: $*" 1>&2
+    exit 1
+}
+
+build_dir=build
+install_prefix=default
+build_type=default
+debug=default
+
+while [ $# -gt 0 ]
+do
+    case $1 in
+        -h|--help) usage;;
+        --prefix) die "missing argument to $1 (see -h)" ;;
+        --prefix=*)
+            install_prefix=${1##*=}
+            # Check if install_prefix is an absolute path and if not, make it
+            # absolute.
+            case $install_prefix in
+                /*) ;;                                      # absolute path
+                *) install_prefix=$(pwd)/$install_prefix ;; # make absolute path
+            esac
+            ;;
+        --build-dir) die "missing argument to $1 (see -h)" ;;
+        --build-dir=*)
+            build_dir=${1##*=}
+            # Check if build_dir is an absolute path and if not, make it
+            # absolute.
+            case $build_dir in
+                /*) ;;                                      # absolute path
+                *) build_dir=$(pwd)/$build_dir ;; # make absolute path
+            esac
+            ;;
+        --debug)
+            debug=yes
+            ;;
+        *) die "unexpected argument: $1";;
+    esac
+    shift
+done
+
+cmake_opts=""
+[ $install_prefix != default ] \
+    && cmake_opts="$cmake_opts -DCMAKE_INSTALL_PREFIX=$install_prefix"
+
+[ $debug != default ] \
+    && cmake_opts="$cmake_opts -DCMAKE_BUILD_TYPE=Debug"
+
+root_dir=$(pwd)
+
+[ -e "$build_dir" ] && rm -r "$build_dir"
+
+mkdir -p "$build_dir"
+cd "$build_dir" || exit 1
+
+[ -e CMakeCache.txt ] && rm CMakeCache.txt
+
+echo "Running with cmake options: $cmake_opts"
+
+cmake "$root_dir" $cmake_opts 2>&1
