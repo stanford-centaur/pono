56197e2f4f42b3625cbbb78c26ab9d3434ed78f2
Makai Mann <makaim@stanford.edu>
2019-10-01 08:38:28 -0700

    Pull smt-switch master now

--- contrib/setup-smt-switch.sh
+++ contrib/setup-smt-switch.sh
@@ -7,7 +7,7 @@ mkdir -p $DEPS
 
 if [ ! -d "$DEPS/smt-switch" ]; then
     cd $DEPS
-    git clone -b btor-update https://github.com/makaimann/smt-switch
+    git clone https://github.com/makaimann/smt-switch
     cd smt-switch
     ./contrib/setup-btor.sh
     ./configure.sh --btor --prefix=local
