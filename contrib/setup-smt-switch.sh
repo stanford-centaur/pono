6250cae924aac76ee0e63d0151aedff61e4e15f7
Makai Mann <makaim@stanford.edu>
2019-10-11 19:02:07 -0700

    Pull the hwmcc19 branch of smt-switch

--- contrib/setup-smt-switch.sh
+++ contrib/setup-smt-switch.sh
@@ -7,7 +7,7 @@ mkdir -p $DEPS
 
 if [ ! -d "$DEPS/smt-switch" ]; then
     cd $DEPS
-    git clone -b btor-smtcomp19 https://github.com/makaimann/smt-switch
+    git clone -b hwmcc19 https://github.com/makaimann/smt-switch
     cd smt-switch
     ./contrib/setup-btor.sh
     ./contrib/setup-msat.sh
