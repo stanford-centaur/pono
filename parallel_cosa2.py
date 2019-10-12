2741feff878aa5a7c0d5ab1eb95b25f3044f4b85
Makai Mann <makaim@stanford.edu>
2019-10-12 11:36:00 -0700

    Improvements to parallel run script

--- parallel_cosa2.py
+++ parallel_cosa2.py
@@ -1,11 +1,13 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import argparse
+import signal
 import subprocess
 import sys
 import time
 
 if __name__ == "__main__":
+
     parser = argparse.ArgumentParser(description="Run BMC and K-induction in parallel")
     parser.add_argument('btor_file')
     parser.add_argument('-k', '--bound', default='200', help='The maximum bound to unroll to')
@@ -16,76 +18,100 @@ if __name__ == "__main__":
     args = parser.parse_args()
     btor_file = args.btor_file
     bound = args.bound
-    verbosity = '1' if args.verbosity else '0'
+    verbosity = args.verbosity
+    verbosity_option = '1' if args.verbosity else '0'
 
-    bmc = subprocess.Popen(['./cosa2', '-e', 'bmc', '-v', verbosity, '-k', bound, btor_file],
-                          stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    bmc_sp = subprocess.Popen(['./cosa2', '-e', 'bmc-sp', '-v', verbosity, '-k', bound, btor_file],
-                              stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    induc = subprocess.Popen(['./cosa2', '-e', 'ind', '-v', verbosity, '-k', bound, btor_file],
-                          stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    interp = subprocess.Popen(['./cosa2', '-e', 'interp', '-v', verbosity, '-k', bound, btor_file],
-                          stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    bmc = subprocess.Popen(['./cosa2', '-e', 'bmc', '-v', verbosity_option, '-k', bound, btor_file],
+                           stdout=subprocess.PIPE)
+    bmc_sp = subprocess.Popen(['./cosa2', '-e', 'bmc-sp', '-v', verbosity_option, '-k', bound, btor_file],
+                              stdout=subprocess.PIPE)
+    induc = subprocess.Popen(['./cosa2', '-e', 'ind', '-v', verbosity_option, '-k', bound, btor_file],
+                          stdout=subprocess.PIPE)
+    interp = subprocess.Popen(['./cosa2', '-e', 'interp', '-v', verbosity_option, '-k', bound, btor_file],
+                          stdout=subprocess.PIPE)
 
+    shutdown = False
+
+    # these stay constant
     interp_processes = set({interp})
+    all_processes = [bmc, bmc_sp, induc, interp]
+
+    # this one is updated
     processes = [bmc, bmc_sp, induc, interp]
+    pos_map = {0: "BMC:",
+               1: "BMC+SimplePath",
+               2: "K-Induction",
+               3: "Interpolant-based"}
 
-    try:
-        while processes:
-            for i, p in enumerate(processes):
-                if p.poll() is not None:
-                    # return code for unknown is 2
-                    # anything higher than that is an error
-                    # keep solving unless there are no more running processes
-                    if p.returncode >= 2:
+    def handle_signal(signum, frame):
+        # send signal recieved to subprocesses
+        global shutdown
+        if not shutdown:
+            shutdown = True
+            for i, proc in enumerate(all_processes):
+                if proc.poll() is None:
+                    proc.terminate()
+
+                global verbosity
+                if verbosity:
+                    # this is too slow
+                    # out, err = proc.communicate()
+                    # print(out.decode('utf-8', errors='replace'))
+
+                    print("{} output:".format(pos_map[i]))
+                    for line in proc.stdout:
+                        print(line.decode('utf-8'), end='')
+                    print()
+            sys.stdout.flush()
+            sys.exit(0)
+
+    signal.signal(signal.SIGINT, handle_signal)
+    signal.signal(signal.SIGTERM, handle_signal)
+
+
+    while not shutdown:
+        for i, p in enumerate(processes):
+            if p.poll() is not None:
+                # return code for unknown is 2
+                # anything higher than that is an error
+                # keep solving unless there are no more running processes
+                if p.returncode >= 2:
+                    processes.remove(p)
+                    # print unknown only if this is the last process
+                    if not processes:
+                        out, _ = p.communicate()
+                        if out is not None:
+                            print(out.decode('utf-8', errors='replace'))
+                            sys.stdout.flush()
+                        shutdown = True
+                else:
+                    # HACK don't return counter-examples from interpolation-based procedure
+                    #      mathsat might return constant arrays in witness which can't be
+                    #      printed in btor2 format
+                    if p in interp_processes and p.returncode == 1:
                         processes.remove(p)
-                        # print unknown only if this is the last process
+                        # this shouldn't happen but let's handle it just in case
                         if not processes:
-                            out, _ = p.communicate()
-                            if out is not None:
-                                print(out.decode('utf-8', errors='replace'))
-                    else:
-                        # HACK don't return counter-examples from interpolation-based procedure
-                        #      mathsat might return constant arrays in witness which can't be
-                        #      printed in btor2 format
-                        if p in interp_processes and p.returncode == 1:
-                            processes.remove(p)
-                            # this shouldn't happen but let's handle it just in case
-                            if not processes:
-                                out, _ = bmc.communicate()
-                                print(out.decode('utf-8', errors='replace'))
-                                break
-                        else:
-                            out, _ = p.communicate()
-                            if out is not None:
-                                print(out.decode('utf-8', errors='replace'))
-                            for pp in processes:
-                                if pp != p:
-                                    pp.terminate()
-                            processes = []
+                            out, _ = bmc.communicate()
+                            print(out.decode('utf-8', errors='replace'))
+                            sys.stdout.flush()
+                            shutdown = True
                             break
+                    else:
+                        out, _ = p.communicate()
+                        if out is not None:
+                            print(out.decode('utf-8', errors='replace'))
+                            sys.stdout.flush()
+                        for pp in processes:
+                            if pp != p:
+                                pp.terminate()
+                        processes = []
+                        shutdown = True
+                        break
 
-            if processes:
-                time.sleep(.01)
+        # just a double check
+        if not processes:
+            shutdown = True
 
-    except KeyboardInterrupt as e:
-        if args.verbosity:
-            # on keyboard interrupt, print the pipes
-            # nothing should have printed yet, otherwise we would have already exited
-            print("Got an interrupt, printing output")
-
-            print("BMC output:")
-            out, _ = bmc.communicate()
-            print(out.decode('utf-8', errors='replace'))
-
-            print("BMC-Simple-Path output:")
-            out, _ = bmc_sp.communicate()
-            print(out.decode('utf-8', errors='replace'))
-
-            print("K-induction output:")
-            out, _ = induc.communicate()
-            print(out.decode('utf-8', errors='replace'))
-
-            print("Interpolation-based model checking output:")
-            out, _ = interp.communicate()
-            print(out.decode('utf-8', errors='replace'))
+        if not shutdown:
+            time.sleep(.01)
