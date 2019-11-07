a1d727dfabbfe2b5af68133ca388f49415671646
Ahmed Irfan <irfan@cs.stanford.edu>
2019-11-06 18:32:52 -0800

    minor

--- core/unroller.cpp
+++ core/unroller.cpp
@@ -2,10 +2,12 @@
 
 #include <assert.h>
 
+using namespace smt;
+
 namespace cosa {
 
 Unroller::Unroller(const RelationalTransitionSystem & ts,
-                   smt::SmtSolver & solver)
+                   SmtSolver & solver)
     : ts_(ts), solver_(solver)
 {
 }
@@ -23,9 +25,9 @@ Unroller::~Unroller()
   }
 }
 
-smt::Term Unroller::at_time(const smt::Term & t, unsigned int k)
+Term Unroller::at_time(const Term & t, unsigned int k)
 {
-  smt::UnorderedTermMap & cache = time_cache_at_time(k);
+  UnorderedTermMap & cache = time_cache_at_time(k);
 
   auto it = cache.find(t);
   if (it != cache.end())
@@ -33,62 +35,61 @@ smt::Term Unroller::at_time(const smt::Term & t, unsigned int k)
     return it->second;
   }
 
-  smt::Term ret = solver_->substitute(t, cache);
+  Term ret = solver_->substitute(t, cache);
   untime_cache_[ret] = t;
   cache[t] = ret;
 
   return ret;
 }
 
-smt::Term Unroller::untime(const smt::Term & t) const
+Term Unroller::untime(const Term & t) const
 {
   return solver_->substitute(t, untime_cache_);
 }
 
-smt::Term Unroller::var_at_time(const smt::Term & v, unsigned int k)
+Term Unroller::var_at_time(const Term & v, unsigned int k)
 {
   assert(v->is_symbolic_const());
   assert(time_var_map_.size() > k);
 
   if (time_var_map_[k]->find(v) != time_var_map_[k]->end())
   {
-    // cache hit
     return time_var_map_[k]->at(v);
   }
 
   std::string name = v->to_string();
   name += "@" + std::to_string(k);
-  smt::Term timed_v = solver_->make_symbol(name, v->get_sort());
+  Term timed_v = solver_->make_symbol(name, v->get_sort());
   time_var_map_[k]->operator[](v) = timed_v;
 
   return timed_v;
 }
 
-smt::UnorderedTermMap & Unroller::time_cache_at_time(unsigned int k)
+UnorderedTermMap & Unroller::time_cache_at_time(unsigned int k)
 {
   while (time_cache_.size() <= k)
   {
-    time_cache_.push_back(new smt::UnorderedTermMap());
+    time_cache_.push_back(new UnorderedTermMap());
   }
 
   while (time_var_map_.size() <= k + 1)
   {
-    time_var_map_.push_back(new smt::UnorderedTermMap());
+    time_var_map_.push_back(new UnorderedTermMap());
   }
 
-  smt::UnorderedTermMap & subst = *(time_cache_[k]);
+  UnorderedTermMap & subst = *(time_cache_[k]);
 
   if (subst.size() == 0)
   {
-    const smt::UnorderedTermSet state_vars = ts_.states();
-    const smt::UnorderedTermSet input_vars = ts_.inputs();
+    const UnorderedTermSet state_vars = ts_.states();
+    const UnorderedTermSet input_vars = ts_.inputs();
 
     for (auto it = state_vars.begin(), end = state_vars.end(); it != end; ++it)
     {
-      smt::Term v = *it;
-      smt::Term vn = ts_.next(v);
-      smt::Term new_v = var_at_time(v, k);
-      smt::Term new_vn = var_at_time(v, k + 1);
+      Term v = *it;
+      Term vn = ts_.next(v);
+      Term new_v = var_at_time(v, k);
+      Term new_vn = var_at_time(v, k + 1);
       subst[v] = new_v;
       subst[vn] = new_vn;
       untime_cache_[new_v] = v;
@@ -96,10 +97,10 @@ smt::UnorderedTermMap & Unroller::time_cache_at_time(unsigned int k)
     }
     for (auto it = input_vars.begin(), end = input_vars.end(); it != end; ++it)
     {
-      smt::Term v = *it;
-      smt::Term vn = ts_.next(v);
-      smt::Term new_v = var_at_time(v, k);
-      smt::Term new_vn = var_at_time(v, k + 1);
+      Term v = *it;
+      Term vn = ts_.next(v);
+      Term new_v = var_at_time(v, k);
+      Term new_vn = var_at_time(v, k + 1);
       subst[v] = new_v;
       subst[vn] = new_vn;
       untime_cache_[new_v] = v;
