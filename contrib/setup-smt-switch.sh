5ee110c85870110f9375f577abba2d19321245dc
Makai Mann <makaim@stanford.edu>
2019-11-06 18:56:55 -0800

    Update smt-switch version

--- contrib/setup-smt-switch.sh
+++ contrib/setup-smt-switch.sh
@@ -4,7 +4,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 DEPS=$DIR/../deps
 
 # get a particular version of smt-switch
-SMT_SWITCH_VERSION=56a4c7877bf734a0d887c15c488efb8df5d7d843
+SMT_SWITCH_VERSION=8c4b1f880d5e8b116a1956dbc433abf5c23438c8
 
 mkdir -p $DEPS
 
