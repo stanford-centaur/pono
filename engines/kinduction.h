54ce958a5eafbe657c7de74e90fe4a835805682f
Ahmed Irfan <43099566+ahmed-irfan@users.noreply.github.com>
2019-12-14 09:02:18 -0800

    License (#8)
    * added license
    
    * moved license to the correct location
    
    * authors file
    
    * added headers in the files
    
    * mention fmt and optionparser in the license file

--- engines/kinduction.h
+++ engines/kinduction.h
@@ -1,3 +1,20 @@
+/*********************                                                        */
+/*! \file 
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Ahmed Irfan, Makai Mann
+ ** This file is part of the cosa2 project.
+ ** Copyright (c) 2019 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved.  See the file LICENSE in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief 
+ **
+ ** 
+ **/
+
+
 #pragma once
 
 #include "prover.h"
