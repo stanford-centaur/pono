ba9c8956be283d1ef823c346a10955b20b73a972
Makai Mann <makaim@stanford.edu>
2020-01-08 16:33:00 -0800

    Fix setup-btor2tools script for upstream changes

--- contrib/setup-btor2tools.sh
+++ contrib/setup-btor2tools.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+BTOR2TOOLS_VERSION=9831f9909fb283752a3d6d60d43613173bd8af42
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 DEPS=$DIR/../deps
 
@@ -9,15 +11,17 @@ if [ ! -d "$DEPS/btor2tools" ]; then
     cd $DEPS
     git clone --depth 1 https://github.com/Boolector/btor2tools.git btor2tools
     cd btor2tools
-    CFLAGS="" ./configure.sh -fPIC
+    git checkout -f $BTOR2TOOLS_VERSION
+    CFLAGS="" ./configure.sh --static
+    cd build
     make -j${NPROC}
     cd $DIR
 else
     echo "$DEPS/btor2tools already exists. If you want to rebuild, please remove it manually."
 fi
 
-if [ -f $DEPS/btor2tools/build/libbtor2parser.a ] ; then \
-    echo "It appears btor2tools was successfully built in $DEPS/btor2tools/build."
+if [ -f $DEPS/btor2tools/build/lib/libbtor2parser.a ] ; then \
+    echo "It appears btor2tools was successfully built in $DEPS/btor2tools/build/lib."
     echo "You may now build cosa2 with: ./configure.sh && cd build && make"
 else
     echo "Building btor2tools failed."
