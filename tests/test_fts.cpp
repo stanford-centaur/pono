b65aaff37fe75ba3ed196e54542dd391aad6f865
Makai Mann <makaim@stanford.edu>
2020-02-26 17:08:31 -0800

    Add another test for functional transition systems

--- tests/test_fts.cpp
+++ tests/test_fts.cpp
@@ -1,9 +1,12 @@
 #include <utility>
 #include <vector>
 
+#include "gtest/gtest.h"
+
 #include "core/fts.h"
 #include "core/unroller.h"
-#include "gtest/gtest.h"
+#include "utils/exceptions.h"
+
 #include "smt-switch/boolector_factory.h"
 #include "smt-switch/smt.h"
 
@@ -28,7 +31,7 @@ class UnitTests : public ::testing::Test,
   Sort bvsort;
 };
 
-TEST_P(UnitTests, FTS)
+TEST_P(UnitTests, FTS_Unroll)
 {
   FunctionalTransitionSystem fts(s);
   Term x = fts.make_state("x", bvsort);
@@ -37,6 +40,14 @@ TEST_P(UnitTests, FTS)
   Unroller u(fts, s);
   Term x0 = u.at_time(x, 0);
   ASSERT_EQ(x0, u.at_time(x, 0));
+}
+
+TEST_P(UnitTests, FTS_Exceptions)
+{
+  FunctionalTransitionSystem fts(s);
+  Term x = fts.make_state("x", bvsort);
+  Term xp1_n = fts.next(s->make_term(BVAdd, x, s->make_term(1, bvsort)));
+  ASSERT_THROW(fts.assign_next(x, xp1_n), CosaException);
 }
 
 INSTANTIATE_TEST_SUITE_P(ParameterizedSolverUnitTests,
