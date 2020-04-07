137e4b468a75f84cebbc1861117ade9d84ad1f18
Hongce Zhang <zhanghongce@126.com>
2020-04-07 16:22:10 -0400

    Update printers/vcd_witness_printer.cpp
    Co-Authored-By: makaimann <makaim@stanford.edu>

--- printers/vcd_witness_printer.cpp
+++ printers/vcd_witness_printer.cpp
@@ -469,7 +469,7 @@ void VCDWitnessPrinter::dump_diff(const smt::UnorderedTermMap & valmap,
         if (prev_pos == valprev.end()) {
           valprev.insert(std::make_pair(addr_pos->second, data ));
           fout << data << " " << addr_pos->second << std::endl;
-          logger.log(0, "{} was not cached before time : {}.",
+          logger.log(1, "{} was not cached before time : {}.",
             sig_array_ptr->full_name+"["+addr+"]", std::to_string(t));
         } else {
           if (prev_pos->second != data) {
