9ca21f4a3d041c7b2402c30dca57048e8fee4f27
yangy96 <yangy96@stanford.edu>
2020-04-10 23:13:03 -0700

    clang format

--- frontends/smv_node.h
+++ frontends/smv_node.h
@@ -11,28 +11,29 @@
 #include "smt-switch/smt.h"
 
 /* node to help construct syntax tree in the parser */
-namespace cosa{
-struct SMVnode {
-      enum Nodetype{TermNode,SortNode};
-      smt::Term tm;
-      smt::Sort st;
-      Nodetype nt;
-      SMVnode (smt::Sort s){
-        nt = SortNode;
-        st = s;
-      }
-      SMVnode (smt::Term t){
-        nt = TermNode;
-        tm = t;
-      }
-      Nodetype getNodeType(){
-         return nt;
-      }
-      smt::Sort getSort(){
-            return st;
-      }
-      smt::Term getTerm(){
-            return tm;
-      }
-}; // struct SMVNode
-} //namespace cosa
+namespace cosa {
+struct SMVnode
+{
+  enum Nodetype
+  {
+    TermNode,
+    SortNode
+  };
+  smt::Term tm;
+  smt::Sort st;
+  Nodetype nt;
+  SMVnode(smt::Sort s)
+  {
+    nt = SortNode;
+    st = s;
+  }
+  SMVnode(smt::Term t)
+  {
+    nt = TermNode;
+    tm = t;
+  }
+  Nodetype getNodeType() { return nt; }
+  smt::Sort getSort() { return st; }
+  smt::Term getTerm() { return tm; }
+};  // struct SMVNode
+}  // namespace cosa
