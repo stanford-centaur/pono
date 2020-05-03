2fea73be8e72ab709807590595d594242c9b9a56
yangy96 <yangy96@stanford.edu>
2020-05-03 16:49:48 -0700

    add signed/unsigned reentrant, fix scanner bugs

--- frontends/smv_encoder.cpp
+++ frontends/smv_encoder.cpp
@@ -1,2 +1,54 @@
 #include "smv_encoder.h"
 
+int cosa::SMVEncoder::parse(std::string filename){
+    std::ifstream ifs;
+    ifs.open(filename);
+  // make sure file is valid:
+    if (!ifs.good()) {
+    std::cout << "NO input file!" << std::endl;
+    exit(-1);
+  }
+  std::istream& s= ifs;
+  cosa::SMVscanner smvscanner(*this);
+  smvscanner.switch_streams(&s);
+  cosa::smvparser parse (smvscanner,*this);
+  return parse();
+}
+
+int cosa::SMVEncoder::parseString(std::string newline){
+  std::istringstream iss(newline);
+  std::istream &s(iss);
+  cosa::SMVscanner smvscanner(*this);
+  smvscanner.switch_streams(&s);
+  cosa::smvparser parse (smvscanner,*this);
+  return parse();
+}
+
+void cosa::SMVEncoder::processCase(){
+    Engine engine = BMC;
+    unsigned int bound = 10;
+    unsigned int verbosity = default_verbosity;
+    Property p(rts, smc);
+    logger.log(1, "Solving property: {}", p.prop());
+    std::cout << "INVARSPEC " << p.prop() << std::endl;
+    std::shared_ptr<Prover> prover;
+    ProverResult r = prover->check_until(bound);
+    if (r == FALSE) {
+      cout << "Find a counterexample" << endl;
+      cout << "b" << prop_idx << endl;
+      vector<UnorderedTermMap> cex;
+      if (prover->witness(cex)) {
+        //print_witness_btor(smv_enc, cex);
+      }
+      return 1;
+    } else if (r == TRUE) {
+      //cout << "unsat" << endl;
+      cout<<"Proved"<<endl;
+      cout << "b" << prop_idx << endl;
+      return 0;
+    } else {
+      cout << "unknown" << endl;
+      cout << "b" << prop_idx << endl;
+      return 2;
+    }
+}
--- frontends/smv_node.h
+++ frontends/smv_node.h
@@ -5,7 +5,6 @@
 #include <map>
 #include <string>
 #include <unordered_map>
-
 #include "assert.h"
 #include "exceptions.h"
 #include "smt-switch/smt.h"
@@ -19,20 +18,41 @@ struct SMVnode
     TermNode,
     SortNode
   };
+  enum BVtype{
+    Signed,
+    Unsigned,
+    BVnot
+  };
   smt::Term tm;
   smt::Sort st;
   Nodetype nt;
+  BVtype bvt;
   SMVnode(smt::Sort s)
   {
     nt = SortNode;
     st = s;
+    bvt = BVnot;
   }
   SMVnode(smt::Term t)
   {
     nt = TermNode;
     tm = t;
+    bvt = BVnot;
+  }
+  SMVnode(smt::Sort s, BVtype bvtype)
+  {
+    nt = SortNode;
+    st = s;
+    bvt = bvtype;
+  }
+  SMVnode(smt::Term t, BVtype bvtype)
+  {
+    nt = TermNode;
+    tm = t;
+    bvt = bvtype;
   }
   Nodetype getNodeType() { return nt; }
+  BVtype getBVType() { return bvt; }
   smt::Sort getSort() { return st; }
   smt::Term getTerm() { return tm; }
 };  // struct SMVNode
