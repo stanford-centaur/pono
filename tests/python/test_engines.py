e2d5fc2122a046267ba914b707baf72defbce4d1
Makai Mann <makaim@stanford.edu>
2020-05-17 16:30:56 -0700

    Create SmtSolvers without logging

--- tests/python/test_engines.py
+++ tests/python/test_engines.py
@@ -53,7 +53,7 @@ def build_simple_alu_fts(s:ss.SmtSolver)->c.Property:
 
 @pytest.mark.parametrize("create_solver", ss.solvers.values())
 def test_bmc(create_solver):
-    s = create_solver()
+    s = create_solver(False)
     s.set_opt('produce-models', 'true')
     s.set_opt('incremental', 'true')
     prop = build_simple_alu_fts(s)
@@ -65,7 +65,7 @@ def test_bmc(create_solver):
 
 @pytest.mark.parametrize("create_solver", ss.solvers.values())
 def test_kind(create_solver):
-    s = create_solver()
+    s = create_solver(False)
     s.set_opt('produce-models', 'true')
     s.set_opt('incremental', 'true')
     prop = build_simple_alu_fts(s)
@@ -91,7 +91,7 @@ def test_interp(solver_and_interpolator):
 
 @pytest.mark.parametrize("create_solver", ss.solvers.values())
 def test_kind_inductive_prop(create_solver):
-    s = create_solver()
+    s = create_solver(False)
     s.set_opt('produce-models', 'true')
     s.set_opt('incremental', 'true')
     prop = build_simple_alu_fts(s)
