7b930379a05f7372fddc7cbdb11390d805aa068b
Makai Mann <makaim@stanford.edu>
2020-06-02 12:07:21 -0700

    Try to find flex headers

--- scripts/travis-mac-install.sh
+++ scripts/travis-mac-install.sh
@@ -8,10 +8,6 @@ if [ "$TRAVIS_OS_NAME" == "osx" ]; then
     export PATH="/usr/local/opt/flex/bin:$PATH"
     export LDFLAGS="-L/usr/local/opt/flex/lib"
     export CPPFLAGS="-I/usr/local/opt/flex/include"
-    # HACK: remove old version of FlexLexer.h that shows up in include path first
-    # see comments here: https://github.com/vmware/cascade/issues/170
-    sudo rm /Library/Developer/CommandLineTools/usr/include/Flex*
-    sudo rm /Applications/Xcode-9.2.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/include/Flex*
 else
     echo "NOT in OSX -- nothing to do"
 fi
