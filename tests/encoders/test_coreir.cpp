5dea754f4c2b540333de8f8066ca9f3647d881ad
Makai Mann <makaim@stanford.edu>
2020-06-04 16:54:49 -0700

    Add a test for CoreIREncoder

---
+++ tests/encoders/test_coreir.cpp
@@ -0,0 +1,44 @@
+#ifdef WITH_COREIR
+#define STRHELPER(A) #A
+#define STRFY(A) STRHELPER(A)
+
+#include <string>
+#include <tuple>
+#include <vector>
+
+#include "gtest/gtest.h"
+
+#include "available_solvers.h"
+#include "core/fts.h"
+#include "frontends/coreir_encoder.h"
+
+using namespace cosa;
+using namespace smt;
+using namespace std;
+
+namespace cosa_tests {
+
+class CoreIRUnitTests : public ::testing::Test,
+                        public ::testing::WithParamInterface<tuple<SolverEnum, string>>
+{};
+
+TEST_P(CoreIRUnitTests, Encode)
+{
+  SmtSolver s = available_solvers().at(get<0>(GetParam()))(false);
+  FunctionalTransitionSystem fts(s);
+  string filename = STRFY(COSA2_SRC_DIR);
+  filename += "/tests/encoders/inputs/coreir/";
+  filename += get<1>(GetParam());
+  cout << "Reading file: " << filename << endl;
+  CoreIREncoder ce(filename, fts);
+}
+
+INSTANTIATE_TEST_SUITE_P(ParameterizedSolverCoreIRUnitTests,
+                         CoreIRUnitTests,
+                         testing::Combine(testing::ValuesIn(available_solver_enums()),
+                                          testing::ValuesIn(vector<string>{
+                                              "counters.json"
+                                                })));
+
+}  // namespace cosa_tests
+#endif
