66aac94b9baae69950777a7e27e135a1feb31c65
yangy96 <yangy96@stanford.edu>
2020-06-22 06:22:48 -0700

    request changes update

--- frontends/smv_encoder.cpp
+++ frontends/smv_encoder.cpp
@@ -4,48 +4,51 @@ using namespace smt;
 using namespace cosa;
 using namespace std;
 
-int cosa::SMVEncoder::parse(std::string filename){
-    std::ifstream ifs;
-    ifs.open(filename);
+int cosa::SMVEncoder::parse(std::string filename)
+{
+  std::ifstream ifs;
+  ifs.open(filename);
   // make sure file is valid:
-    if (!ifs.good()) {
+  if (!ifs.good()) {
     std::cout << "NO input file!" << std::endl;
     exit(-1);
   }
-  std::istream& s= ifs;
+  std::istream & s = ifs;
   cosa::SMVscanner smvscanner(*this);
   smvscanner.switch_streams(&s);
-  cosa::smvparser parse (smvscanner,*this);
+  cosa::smvparser parse(smvscanner, *this);
   return parse();
 }
 // parse string
-int cosa::SMVEncoder::parseString(std::string newline){
+int cosa::SMVEncoder::parseString(std::string newline)
+{
   std::istringstream iss(newline);
-  std::istream &s(iss);
+  std::istream & s(iss);
   cosa::SMVscanner smvscanner(*this);
   smvscanner.switch_streams(&s);
-  cosa::smvparser parse (smvscanner,*this);
+  cosa::smvparser parse(smvscanner, *this);
   return parse();
 }
-//case condition check preprocess
-void cosa::SMVEncoder::processCase(){
+// case condition check preprocess
+void cosa::SMVEncoder::processCase()
+{
   std::future_status status;
+  solver_->push();
   for (std::pair<int, smt::Term> element : casecheck_) {
-    solver_->push();
     Term bad_ = solver_->make_term(smt::PrimOp::Not, element.second);
     solver_->assert_formula(bad_);
     auto fut = std::async(
         launch::async,
         [](smt::SmtSolver solver_) {
           Result r = solver_->check_sat();
-          return r.is_sat();
+          return r.is_unsat();
         },
         solver_);
     std::future_status status;
     status = fut.wait_for(std::chrono::seconds(5));
     while (status != std::future_status::timeout) {
       if (status == std::future_status::ready) {
-        if (fut.get() == false) {
+        if (fut.get()) {
           rts_.constrain_trans(casestore_[element.first]);
           break;
         } else {
@@ -53,8 +56,9 @@ void cosa::SMVEncoder::processCase(){
         }
       }
     }
-    if (status == std::future_status::timeout)
+    if (status == std::future_status::timeout) {
       throw CosaException("case timeout check error");
-    solver_->pop();
+    }
   }
+  solver_->pop();
 }
