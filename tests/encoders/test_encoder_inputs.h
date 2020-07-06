5815584cb3586b4cbd9239d10a8366cd9e168ef7
Makai Mann <makaim@stanford.edu>
2020-07-06 13:00:53 -0700

    Add messing header file that lists encoder inputs

---
+++ tests/encoders/test_encoder_inputs.h
@@ -0,0 +1,20 @@
+#pragma once
+
+// used in the individual tests to recover string paths from macros
+#define STRHELPER(A) #A
+#define STRFY(A) STRHELPER(A)
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+const vector<string> coreir_inputs({ "counters.json",
+                                     "WrappedPE_nofloats.json",
+                                     "SimpleALU.json" });
+
+const vector<string> btor2_inputs({ "counter.btor",
+                                    "counter-true.btor",
+                                    "mem.btor",
+                                    "ridecore.btor",
+                                    "state2input.btor" });
