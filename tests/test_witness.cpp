24007ed9b29d451b2af045463dffbebe4377bbf5
Makai Mann <makaim@stanford.edu>
2020-07-10 07:01:57 -0700

    Add test for witness generation

--- tests/test_witness.cpp
+++ tests/test_witness.cpp
@@ -53,6 +53,45 @@ TEST_P(WitnessUnitTests, SimpleDefaultSolver)
   ASSERT_EQ(witness[8][x], eight);
 }
 
+TEST_P(WitnessUnitTests, ArraysDefaultSolver)
+{
+  // use default solver
+  FunctionalTransitionSystem fts;
+  Sort bvsort4 = fts.make_sort(BV, 4);
+  Sort bvsort8 = fts.make_sort(BV, 8);
+  Sort arrsort = fts.make_sort(ARRAY, bvsort4, bvsort8);
+
+  Term arr = fts.make_statevar("arr", arrsort);
+  Term idx = fts.make_statevar("idx", bvsort4);
+  Term x = fts.make_statevar("x", bvsort4);
+  Term data = fts.make_statevar("data", bvsort8);
+  Term ext_idx = fts.make_term(Op(Zero_Extend, 4), idx);
+
+  Term constarr0 = fts.make_term(fts.make_term(0, bvsort8), arrsort);
+  fts.constrain_init(fts.make_term(Equal, arr, constarr0));
+  fts.constrain_init(fts.make_term(Equal, idx, fts.make_term(0, bvsort4)));
+
+  fts.assign_next(idx, fts.make_term(BVAdd, idx, fts.make_term(2, bvsort4)));
+  fts.assign_next(arr, fts.make_term(Store, arr, idx, data));
+  fts.add_invar(fts.make_term(BVUle, data, ext_idx));
+
+  Term ten = fts.make_term(10, bvsort8);
+  Term prop_term = fts.make_term(Distinct, fts.make_term(Select, arr, x), ten);
+  Property prop(fts, prop_term);
+
+  Bmc bmc(prop, GetParam());
+  ProverResult r = bmc.check_until(6);
+  ASSERT_EQ(r, FALSE);
+
+  vector<UnorderedTermMap> witness;
+  bool ok = bmc.witness(witness);
+  ASSERT_TRUE(ok);
+  ASSERT_EQ(witness.size(), 7);
+  ASSERT_NE(witness[6].find(arr), witness[6].end());
+  ASSERT_NE(witness[6].find(x), witness[6].end());
+  ASSERT_EQ(witness[6][x], fts.make_term(10, bvsort4));
+}
+
 INSTANTIATE_TEST_SUITE_P(ParameterizedWitnessUnitTests,
                          WitnessUnitTests,
                          testing::ValuesIn(available_solver_enums()));
