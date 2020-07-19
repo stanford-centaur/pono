351baeb463644d2360b137f22985ed9c82e259ce
Makai Mann <makaim@stanford.edu>
2020-07-19 05:39:45 -0700

    Use g++ on mac

--- scripts/travis-mac-install.sh
+++ scripts/travis-mac-install.sh
@@ -3,7 +3,7 @@
 if [ "$TRAVIS_OS_NAME" == "osx" ]; then
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     export PATH=/usr/local/bin:/usr/local/sbin:$PATH
-    brew install gnu-getopt gmp flex
+    brew install gnu-getopt gmp flex gcc@7
     brew upgrade flex
     # Apple ships old Flex & Bison, so Homebrew versions should be used.
     export PATH="/usr/local/opt/flex/bin:$PATH"
