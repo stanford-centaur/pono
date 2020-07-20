29537317b43cd1de9d320663f73100286846486e
Makai Mann <makaim@stanford.edu>
2020-07-20 07:44:58 -0700

    Add initial fault injector

---
+++ modifiers/fault_injector.cpp
@@ -0,0 +1,65 @@
+/*********************                                                        */
+/*! \file fault_injector.cpp
+** \verbatim
+** Top contributors (to current version):
+**   Makai Mann
+** This file is part of the pono project.
+** Copyright (c) 2019 by the authors listed in the file AUTHORS
+** in the top-level source directory) and their institutional affiliations.
+** All rights reserved.  See the file LICENSE in the top-level source
+** directory for licensing information.\endverbatim
+**
+** \brief Modifies a transition system such that faults can occur on state
+**        variables at each timestep. This assumes a functional model, and
+**        inserts a mux and a boolean FAULT variable for each state element.
+**        If the fault variable is true, it can get a non-deterministic value
+**
+**/
+
+#include "fault_injector.h"
+#include "logger.h"
+
+using namespace std;
+using namespace smt;
+
+namespace pono {
+
+void FaultInjector::do_fault_injection()
+{
+  const UnorderedTermMap & state_updates = fts_.state_updates();
+  SmtSolver & solver = faulty_fts_.solver();
+
+  Sort boolsort = solver->make_sort(BOOL);
+  Term s;
+  Term faultsig;
+  Term faultsel;
+  Term faultval;
+  for (auto elem : state_updates) {
+    s = elem.first;
+
+    faultsel =
+        faulty_fts_.make_inputvar("faultsel_" + s->to_string(), boolsort);
+    state2faultsel_[s] = faultsel;
+    faultsel2state_[faultsel] = s;
+
+    faultsig = faulty_fts_.make_statevar("FAULT_" + s->to_string(), boolsort);
+    state2faultsig_[s] = faultsig;
+    faultsig2state_[faultsig] = s;
+
+    faulty_fts_.constrain_init(
+        solver->make_term(Equal, faultsig, solver->make_term(false)));
+    faulty_fts_.assign_next(faultsig,
+                            solver->make_term(Or, faultsig, faultsel));
+
+    faultval =
+        faulty_fts_.make_inputvar("faultval_" + s->to_string(), s->get_sort());
+    state2faultval_[s] = faultval;
+    faultval2state_[faultval] = s;
+
+    faulty_fts_.assign_next(
+        s, solver->make_term(Ite, faultsig, faultval, elem.second));
+    fault_sigs_.push_back(faultsig);
+  }
+}
+
+}  // namespace pono
---
+++ modifiers/fault_injector.h
@@ -0,0 +1,64 @@
+/*********************                                                        */
+/*! \file fault_injector.h
+** \verbatim
+** Top contributors (to current version):
+**   Makai Mann
+** This file is part of the pono project.
+** Copyright (c) 2019 by the authors listed in the file AUTHORS
+** in the top-level source directory) and their institutional affiliations.
+** All rights reserved.  See the file LICENSE in the top-level source
+** directory for licensing information.\endverbatim
+**
+** \brief Modifies a transition system such that faults can occur on state
+**        variables at each timestep. This assumes a functional model, and
+**        inserts a mux and a boolean FAULT variable for each state element.
+**        If the fault variable is true, it can get a non-deterministic value
+**
+**/
+
+#pragma once
+
+#include "smt-switch/smt.h"
+
+#include "exceptions.h"
+#include "fts.h"
+
+namespace pono {
+
+class FaultInjector
+{
+ public:
+  FaultInjector(TransitionSystem & fts) : fts_(fts), faulty_fts_(fts.solver())
+  {
+    if (!fts.is_functional()) {
+      throw PonoException("Can only do fault injection on functional systems.");
+    }
+    do_fault_injection();
+  };
+
+  FunctionalTransitionSystem faulty_transition_system() const
+  {
+    return faulty_fts_;
+  };
+
+  smt::TermVec fault_sigs() const { return fault_sigs_; };
+
+ protected:
+  void do_fault_injection();
+
+  TransitionSystem & fts_;
+  FunctionalTransitionSystem faulty_fts_;
+
+  smt::UnorderedTermMap state2faultsel_;
+  smt::UnorderedTermMap faultsel2state_;
+
+  smt::UnorderedTermMap state2faultval_;
+  smt::UnorderedTermMap faultval2state_;
+
+  smt::UnorderedTermMap state2faultsig_;
+  smt::UnorderedTermMap faultsig2state_;
+
+  smt::TermVec fault_sigs_;
+};
+
+}  // namespace pono
