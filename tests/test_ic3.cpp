a1abccb4b08fbf86f4ddc1817c230173b81a15d6
Makai Mann <makaim@stanford.edu>
2020-07-29 09:38:51 -0700

    Add false version of simple IC3 test

--- tests/test_ic3.cpp
+++ tests/test_ic3.cpp
@@ -52,6 +52,28 @@ TEST_P(IC3UnitTests, SimpleSystemSafe)
   ASSERT_EQ(r, TRUE);
 }
 
+TEST_P(IC3UnitTests, SimpleSystemUnsafe)
+{
+  FunctionalTransitionSystem fts(s);
+  Term s1 = fts.make_statevar("s1", boolsort);
+  Term s2 = fts.make_statevar("s2", boolsort);
+
+  // INIT !s1 & s2
+  fts.constrain_init(s->make_term(Not, s1));
+  fts.constrain_init(s2);
+
+  // TRANS next(s1) = (s1 | s2)
+  // TRANS next(s2) = s2
+  fts.assign_next(s1, s->make_term(Or, s1, s2));
+  fts.assign_next(s2, s2);
+
+  Property p(fts, s->make_term(Not, s1));
+
+  ModelBasedIC3 mbic3(p, s);
+  ProverResult r = mbic3.prove();
+  ASSERT_EQ(r, FALSE);
+}
+
 INSTANTIATE_TEST_SUITE_P(ParameterizedSolverIC3UnitTests,
                          IC3UnitTests,
                          testing::ValuesIn(available_solver_enums()));
