e66f6727986c01d115a1bce1282bb9d61c040eb1
Makai Mann <makaim@stanford.edu>
2020-09-16 14:26:57 -0700

    Make Property non-const in MBIC3

--- engines/mbic3.cpp
+++ engines/mbic3.cpp
@@ -87,7 +87,7 @@ Conjunction::Conjunction(const smt::SmtSolver & solver,
 
 // main implementations
 
-ModelBasedIC3::ModelBasedIC3(const Property & p, SolverEnum se)
+ModelBasedIC3::ModelBasedIC3(Property & p, SolverEnum se)
     : super(p, se),
       true_(solver_->make_term(true)),
       false_(solver_->make_term(false))
@@ -97,7 +97,7 @@ ModelBasedIC3::ModelBasedIC3(const Property & p, SolverEnum se)
   initialize();
 }
 
-ModelBasedIC3::ModelBasedIC3(const Property & p, const SmtSolver & slv)
+ModelBasedIC3::ModelBasedIC3(Property & p, const SmtSolver & slv)
     : super(p, slv),
       true_(solver_->make_term(true)),
       false_(solver_->make_term(false))
@@ -106,7 +106,7 @@ ModelBasedIC3::ModelBasedIC3(const Property & p, const SmtSolver & slv)
 }
 
 ModelBasedIC3::ModelBasedIC3(const PonoOptions & opt,
-                             const Property & p,
+                             Property & p,
                              const SolverEnum se)
     : super(opt, p, se),
       true_(solver_->make_term(true)),
@@ -118,7 +118,7 @@ ModelBasedIC3::ModelBasedIC3(const PonoOptions & opt,
 }
 
 ModelBasedIC3::ModelBasedIC3(const PonoOptions & opt,
-                             const Property & p,
+                             Property & p,
                              const SmtSolver & slv)
     : super(opt, p, slv),
       true_(solver_->make_term(true)),
