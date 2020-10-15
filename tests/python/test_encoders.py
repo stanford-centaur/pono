4edd51fe03665effa3ea4cf5b8f553dc6aa2ad2e
makaimann <makaim@stanford.edu>
2020-10-15 14:13:25 -0700

    Check that list of files is nonempty and fix assertion typo (#99)

--- tests/python/test_encoders.py
+++ tests/python/test_encoders.py
@@ -17,13 +17,16 @@ from pathlib import Path
 def test_coreir(create_solver):
     file_dir = Path(os.path.dirname(__file__))
     coreir_inputs_directory = file_dir / Path("../encoders/inputs/coreir")
-    for f in coreir_inputs_directory.glob("*.json"):
+    coreir_files = list(coreir_inputs_directory.glob("*.json"))
+    assert coreir_files
+    for f in coreir_files:
         print("Encoding:", f)
         context = coreir.Context()
+        context.load_library("commonlib")
         top_mod = context.load_from_file(str(f))
 
         solver = create_solver(False)
         rts = c.RelationalTransitionSystem(solver)
         ce = c.CoreIREncoder(top_mod, rts)
         t = solver.make_term(True)
-        assert ce.trans != t, "Expecting a non-empty transition relation"
+        assert rts.trans != t, "Expecting a non-empty transition relation"
