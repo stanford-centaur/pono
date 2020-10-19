30c0a94b8348ca7b5b2d670c278067af6933c6fa
Makai Mann <makaim@stanford.edu>
2020-10-19 10:58:16 -0700

    Add a simple unsafe test

--- tests/test_msat_ic3ia.cpp
+++ tests/test_msat_ic3ia.cpp
@@ -49,6 +49,23 @@ TEST_P(MsatIC3IAUnitTests, IntCounterSafe)
   EXPECT_EQ(res, ProverResult::TRUE);
 }
 
+TEST_P(MsatIC3IAUnitTests, IntCounterUnsafe)
+{
+  RelationalTransitionSystem rts(s);
+  Term c = rts.make_statevar("c", intsort);
+
+  rts.constrain_init(rts.make_term(Equal, c, rts.make_term(0, intsort)));
+  rts.constrain_trans(rts.make_term(Implies,
+                                    rts.make_term(Lt, c, rts.make_term(10, intsort)),
+                                    rts.make_term(Equal, rts.next(c), rts.make_term(Plus, c, rts.make_term(1, intsort)))));
+
+  Property p(rts, rts.make_term(Ge, c, rts.make_term(0, intsort)));
+
+  MsatIC3IA msat_ic3ia(p, s);
+  ProverResult res = msat_ic3ia.prove();
+  EXPECT_EQ(res, ProverResult::FALSE);
+}
+
 INSTANTIATE_TEST_SUITE_P(ParameterizedSolverMsatIC3IAUnitTests,
                          MsatIC3IAUnitTests,
                          testing::ValuesIn({ MSAT }));
