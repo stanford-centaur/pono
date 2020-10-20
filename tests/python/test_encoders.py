6399fec6743f54ac7dd8783aa224bc80f812c09d
makaimann <makaim@stanford.edu>
2020-10-20 13:36:10 -0700

    Minor fixes for building with CoreIR (#106)
    * Don't run coreir tests if not built with it
    
    * Fix for mac when building coreir

--- tests/python/test_encoders.py
+++ tests/python/test_encoders.py
@@ -1,7 +1,7 @@
 import os
 import pytest
 import smt_switch as ss
-import pono as c
+import pono
 
 try:
     import coreir
@@ -13,6 +13,7 @@ from pathlib import Path
 
 
 @pytest.mark.skipif(not COREIR_AVAILABLE, reason="Requires coreir python bindings")
+@pytest.mark.skipif(not hasattr(pono, "CoreIREncoder"), reason="Requires building with coreir")
 @pytest.mark.parametrize("create_solver", ss.solvers.values())
 def test_coreir(create_solver):
     file_dir = Path(os.path.dirname(__file__))
@@ -26,7 +27,7 @@ def test_coreir(create_solver):
         top_mod = context.load_from_file(str(f))
 
         solver = create_solver(False)
-        rts = c.RelationalTransitionSystem(solver)
-        ce = c.CoreIREncoder(top_mod, rts)
+        rts = pono.RelationalTransitionSystem(solver)
+        ce = pono.CoreIREncoder(top_mod, rts)
         t = solver.make_term(True)
         assert rts.trans != t, "Expecting a non-empty transition relation"
