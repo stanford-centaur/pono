18d768b4214c3ee22b8f38909a296f301392ce3c
Makai Mann <makaim@stanford.edu>
2020-10-20 09:46:30 -0700

    Increment reached_k_ before returning false in InterpolantMC

--- engines/interpolantmc.cpp
+++ engines/interpolantmc.cpp
@@ -197,6 +197,8 @@ bool InterpolantMC::step(int i)
       if (!r.is_sat()) {
         throw PonoException("Internal error: Expecting satisfiable result");
       }
+      // increment reached_k_ so that witness goes up to the bad state
+      ++reached_k_;
       return false;
     }
     else if (r.is_unknown())
