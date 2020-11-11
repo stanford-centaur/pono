9d2d1616d57db2d24a55c8a6aa3293dee0465548
Makai Mann <makaim@stanford.edu>
2020-11-10 17:57:25 -0800

    Fix version number

--- contrib/wheels/build_wheel.py
+++ contrib/wheels/build_wheel.py
@@ -107,7 +107,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='pono',
-    version='0.0.9',
+    version='0.1.0',
     author='Makai Mann',
     ext_modules=[CMakeExtension('pono')],
     cmdclass=dict(build_ext=CMakeBuild),
