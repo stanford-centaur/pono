9da59eee33f26537513f65402ae69c6676cadb39
Makai Mann <makaim@stanford.edu>
2020-11-11 10:39:37 -0800

    Increment version number

--- contrib/wheels/build_wheel.py
+++ contrib/wheels/build_wheel.py
@@ -107,7 +107,7 @@ class CMakeBuild(build_ext):
 
 setup(
     name='pono',
-    version='0.1.0',
+    version='0.1.1',
     author='Makai Mann',
     ext_modules=[CMakeExtension('pono')],
     cmdclass=dict(build_ext=CMakeBuild),
