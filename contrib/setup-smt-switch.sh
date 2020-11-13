f0cacde79ecf6efedf8214dc78bc514975cabdbd
Makai Mann <makaim@stanford.edu>
2020-11-13 08:11:21 -0800

    Pull master branch of smt-switch

--- contrib/setup-smt-switch.sh
+++ contrib/setup-smt-switch.sh
@@ -3,7 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 DEPS=$DIR/../deps
 
-SMT_SWITCH_VERSION=829b1f29b2a1529e406918c3136bfde4137a7c83
+SMT_SWITCH_VERSION=cea17f272712b93e7df96a8b194b2c21cd599b15
 
 usage () {
     cat <<EOF
@@ -59,11 +59,9 @@ mkdir -p $DEPS
 
 if [ ! -d "$DEPS/smt-switch" ]; then
     cd $DEPS
-    # TEMPORARILY pull a branch
-    # TODO: put this back
-    git clone -b refactor-get-symbols https://github.com/makaimann/smt-switch
+    git clone https://github.com/makaimann/smt-switch
     cd smt-switch
-    # git checkout -f $SMT_SWITCH_VERSION
+    git checkout -f $SMT_SWITCH_VERSION
     ./contrib/setup-btor.sh
     if [ $cvc4_home = default ]; then
         ./contrib/setup-cvc4.sh
