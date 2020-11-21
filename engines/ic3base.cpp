f9113449aea3540dac7bbccc3e09818d01d262d4
Makai Mann <makaim@stanford.edu>
2020-11-20 18:45:14 -0800

    WIP: start moving implementation to IC3Base

--- engines/ic3base.cpp
+++ engines/ic3base.cpp
@@ -15,9 +15,11 @@
 **
 **/
 
+#include "engines/ic3base.h"
+
 #include <type_traits>
 
-#include "engines/ic3base.h"
+#include "assert.h"
 
 using namespace smt;
 using namespace std;
@@ -25,13 +27,13 @@ using namespace std;
 namespace pono {
 
 IC3Base::IC3Base(Property & p, smt::SolverEnum se, IC3UnitCreator ic)
-    : super(p, se), mk_unit(ic)
+    : super(p, se), mk_unit(ic), solver_context_(0)
 {
   initialize();
 }
 
 IC3Base::IC3Base(Property & p, const smt::SmtSolver & s, IC3UnitCreator ic)
-    : super(p, s), mk_unit(ic)
+    : super(p, s), mk_unit(ic), solver_context_(0)
 {
   initialize();
 }
@@ -40,7 +42,7 @@ IC3Base::IC3Base(const PonoOptions & opt,
                  Property & p,
                  smt::SolverEnum se,
                  IC3UnitCreator ic)
-    : super(opt, p, se), mk_unit(ic)
+    : super(opt, p, se), mk_unit(ic), solver_context_(0)
 {
   initialize();
 }
@@ -48,7 +50,7 @@ IC3Base::IC3Base(const PonoOptions & opt,
                  Property & p,
                  const smt::SmtSolver & s,
                  IC3UnitCreator ic)
-    : super(opt, p, s), mk_unit(ic)
+    : super(opt, p, s), mk_unit(ic), solver_context_(0)
 {
   initialize();
 }
@@ -60,6 +62,220 @@ void IC3Base::initialize()
   //       need to think about multiple levels of inheritance and where the
   //       responsibility for calling initialize belongs
   super::initialize();
+
+  solver_true_ = solver_->make_term(true);
+}
+
+ProverResult IC3Base::check_until(int k) { throw PonoException("NYI"); }
+
+bool IC3Base::witness(std::vector<smt::UnorderedTermMap> & out)
+{
+  throw PonoException("NYI");
+}
+
+// Protected Methods
+
+bool IC3Base::intersects_bad() { throw PonoException("NYI"); }
+
+ProverResult IC3Base::step(int i) { throw PonoException("NYI"); }
+
+ProverResult IC3Base::step_0() { throw PonoException("NYI"); }
+
+// Helper methods
+
+bool IC3Base::block_all() { throw PonoException("NYI"); }
+
+bool IC3Base::block(const IC3Goal & pg) { throw PonoException("NYI"); }
+
+bool IC3Base::propagate(size_t i)
+{
+  assert(i + 1 < frames_.size());
+
+  unordered_set<size_t> indices_to_remove;
+  const vector<IC3Unit> & Fi = frames_.at(i);
+
+  push_solver_context();
+  assert_frame_labels(i);
+  assert_trans_label();
+
+  for (size_t j = 0; j < Fi.size(); ++j) {
+    const Term & t = Fi.at(j)->get_term();
+
+    // Relative inductiveness check
+    // Check F[i] /\ t /\ T /\ -t'
+    // NOTE: asserting t is redundant because t \in F[i]
+    push_solver_context();
+    solver_->assert_formula(solver_->make_term(Not, ts_.next(t)));
+
+    Result r = solver_->check_sat();
+    assert(!r.is_unknown());
+    if (r.is_unsat()) {
+      // mark for removal
+      indices_to_remove.insert(j);
+      // add to next frame
+      constrain_frame(i + 1, Fi.at(j));
+    }
+
+    pop_solver_context();
+  }
+
+  pop_solver_context();
+
+  // keep invariant that terms are kept at highest frame
+  // where they are known to hold
+  // need to remove some terms from the current frame
+  vector<IC3Unit> new_frame_i;
+  new_frame_i.reserve(Fi.size() - indices_to_remove.size());
+  for (size_t j = 0; j < Fi.size(); ++j) {
+    if (indices_to_remove.find(j) == indices_to_remove.end()) {
+      new_frame_i.push_back(Fi.at(j));
+    }
+  }
+
+  frames_[i] = new_frame_i;
+
+  return new_frame_i.empty();
+}
+
+void IC3Base::push_frame()
+{
+  assert(frame_labels_.size() == frames_.size());
+  // pushes an empty frame
+  frame_labels_.push_back(
+      solver_->make_symbol("__frame_label_" + std::to_string(frames_.size()),
+                           solver_->make_sort(BOOL)));
+  frames_.push_back({});
+}
+
+void IC3Base::constrain_frame(size_t i, const IC3Unit & constraint)
+{
+  assert(i < frame_labels_.size());
+  assert(frame_labels_.size() == frames_.size());
+  solver_->assert_formula(
+      solver_->make_term(Implies, frame_labels_.at(i), constraint->get_term()));
+  frames_.at(i).push_back(constraint);
+}
+
+void IC3Base::assert_frame_labels(size_t i) const
+{
+  // never expecting to assert a frame at base context
+  assert(solver_context_ > 0);
+  assert(frame_labels_.size() == frames_.size());
+  Term assump;
+  for (size_t j = 0; j < frame_labels_.size(); ++j) {
+    assump = frame_labels_[j];
+    if (j < i) {
+      // optimization: disable the unused constraints
+      // by asserting the negated label
+      assump = solver_->make_term(Not, assump);
+    }
+    assert(assump);  // assert that it's non-null
+    solver_->assert_formula(assump);
+  }
+}
+
+Term IC3Base::get_frame(size_t i) const
+{
+  // TODO: decide if frames should hold IC3Units or terms
+  //       need to special case initial state if using IC3Units
+  if (i == 0) {
+    // F[0] is always the initial states constraint
+    return ts_.init();
+  }
+
+  Term res = solver_true_;
+  for (size_t j = i; j < frames_.size(); ++j) {
+    for (auto u : frames_[j]) {
+      res = solver_->make_term(And, res, u->get_term());
+    }
+  }
+  return res;
+}
+
+void IC3Base::assert_trans_label() const
+{
+  // shouldn't be a scenario where trans is asserted at base context
+  // just because of how IC3 works
+  assert(solver_context_ > 0);
+  solver_->assert_formula(trans_label_);
+}
+
+bool IC3Base::has_proof_goals() const { return !proof_goals_.empty(); }
+
+IC3Goal IC3Base::get_next_proof_goal()
+{
+  assert(has_proof_goals());
+  IC3Goal pg = std::move(proof_goals_.back());
+  proof_goals_.pop_back();
+  return pg;
+}
+
+void IC3Base::add_proof_goal(const IC3Unit & c, size_t i, unique_ptr<IC3Goal> n)
+{
+  // IC3Unit aligned with frame so proof goal should be negated
+  // e.g. for bit-level IC3, IC3Unit is a Clause and the proof
+  // goal should be a Cube
+  assert(c->is_negated());
+  proof_goals_.push_back(IC3Goal(c, i, std::move(n)));
+}
+
+bool IC3Base::intersects(const Term & A, const Term & B)
+{
+  push_solver_context();
+  solver_->assert_formula(A);
+  solver_->assert_formula(B);
+  Result r = solver_->check_sat();
+  pop_solver_context();
+  return r.is_sat();
+}
+
+bool IC3Base::intersects_initial(const Term & t)
+{
+  return intersects(init_label_, t);
+}
+
+void IC3Base::fix_if_intersects_initial(TermVec & to_keep, const TermVec & rem)
+{
+  // TODO use the UnsatCoreReducer from from smt-switch
+  throw PonoException("NYI");
+}
+
+size_t IC3Base::find_highest_frame(size_t i, const IC3Unit & u)
+{
+  assert(!u->is_negated());
+  Term c = u->get_term();
+  push_solver_context();
+  solver_->assert_formula(c);
+  solver_->assert_formula(solver_->make_term(Not, ts_.next(c)));
+  assert_trans_label();
+
+  Result r;
+  size_t j;
+  for (j = i; j + 1 < frames_.size(); ++j) {
+    push_solver_context();
+    assert_frame_labels(j);
+    r = solver_->check_sat();
+    pop_solver_context();
+    if (r.is_sat()) {
+      break;
+    }
+  }
+
+  pop_solver_context();
+
+  return j;
+}
+
+void IC3Base::push_solver_context()
+{
+  solver_->push();
+  solver_context_++;
+}
+
+void IC3Base::pop_solver_context()
+{
+  solver_->pop();
+  solver_context_--;
 }
 
 }  // namespace pono
--- engines/ic3base.h
+++ engines/ic3base.h
@@ -20,11 +20,11 @@
 
 namespace pono {
 
-class IC3Unit
+class AbstractIC3Unit
 {
  public:
-  IC3Unit(const smt::TermVec & c) : children_(c), negated_(false) {}
-  virtual ~IC3Unit() {}
+  AbstractIC3Unit(const smt::TermVec & c) : children_(c), negated_(false) {}
+  virtual ~AbstractIC3Unit() {}
 
   /** Get a single term representation
    *  Depends on the unit, e.g. a Disjunction unit would be
@@ -45,25 +45,27 @@ class IC3Unit
   smt::TermVec children_;
   bool negated_;
 
-  /** Check if this is a valid instance of this type of IC3Unit
+  /** Check if this is a valid instance of this type of AbstractIC3Unit
    *  e.g. a Clause would make sure all the children are literals
    */
   virtual bool check_valid() const = 0;
 };
 
+using IC3Unit = std::shared_ptr<AbstractIC3Unit>;
+
 typedef std::unique_ptr<IC3Unit> (*IC3UnitCreator)(const smt::TermVec & terms);
 
-// TODO change back to ProofGoal once refactor is done
+// TODO change back to IC3Goal once refactor is done
 // don't want to clash with name in MBIC3 for now
 struct IC3Goal
 {
   // based on open-source ic3ia ProofObligation
-  std::unique_ptr<IC3Unit> target;
+  IC3Unit target;
   size_t idx;
   std::unique_ptr<IC3Goal> next;
 
-  IC3Goal(std::unique_ptr<IC3Unit> u, size_t i, std::unique_ptr<IC3Goal> n)
-      : target(std::move(u)), idx(i), next(std::move(n))
+  IC3Goal(IC3Unit u, size_t i, std::unique_ptr<IC3Goal> n)
+      : target(u), idx(i), next(std::move(n))
   {
   }
 };
@@ -91,9 +93,19 @@ class IC3Base : public Prover
 
   void initialize() override;
 
+  ProverResult check_until(int k) override;
+
+  bool witness(std::vector<smt::UnorderedTermMap> & out) override;
+
  protected:
   ///< a function to create an IC3Unit
   IC3UnitCreator mk_unit;
+
+  ///< keeps track of the current context-level of the solver
+  // NOTE: if solver is passed in, it could be off
+  //       currently no way to check
+  //       then solver_context_ is relative to the starting context
+  size_t solver_context_;
 
   ///< the frames data structure.
   ///< a vector of the given Unit template
@@ -107,6 +119,174 @@ class IC3Base : public Prover
   smt::Term init_label_;       ///< label to activate init
   smt::Term trans_label_;      ///< label to activate trans
   smt::TermVec frame_labels_;  ///< labels to activate frames
+
+  // useful terms
+  smt::Term solver_true_;
+
+  // TODO Make sure all comments are updated!
+
+  // ********************************** Main Methods
+  // ******************************
+
+  // ********************************** Virtual Methods
+  // These methods should be implemented by a derived class for a particular
+  // "flavor" of IC3 in accordance with the associated IC3Unit
+
+  /** Attempt to generalize before adding to a frame
+   *  The standard approach is inductive generalization
+   *  @requires !get_predecessor(i, c, _)
+   *  @param i the frame number to generalize it against
+   *  @param c the cube to find a general predecessor for
+   *  @return a new term P
+   *  @ensures P -> !c /\ F[i-1] /\ P /\ T /\ !P' is unsat
+   */
+  virtual smt::Term inductive_generalization(size_t i, const IC3Unit & c) = 0;
+
+  /** Get the predecessor of a cube c in frame i
+   *  aka see if c is reachable from frame i-1
+   *  @requires c -> F[i]
+   *  @param i the frame number
+   *  @param t the term to check
+   *  @param cti the cube to populate with a cti
+   *  @return true iff c is reachable from the frame i
+   *  @ensures returns true  : pred -> F[i-1] /\ (pred, c) \in [T]
+   *           returns false : pred unchanged, F[i-1] /\ T /\ c' is unsat
+   */
+  virtual bool get_predecessor(size_t i,
+                               const IC3Unit & c,
+                               IC3Unit & out_pred) = 0;
+
+  /** Generalize a counterexample
+   *  @requires get_predecessor(i, c)
+   *  @param i the frame number
+   *  @param c the cube to generalize
+   *  @return a new cube d
+   *  @ensures d -> F[i-1] /\ forall s \in [d] exists s' \in [c]. (d,c) \in [T]
+   */
+  virtual IC3Unit generalize_predecessor(size_t i, const IC3Unit & c) = 0;
+
+  // ********************************** Common Methods
+  // These methods are common to all flavors of IC3 currently implemented
+
+  /** Check if a transition from the second to last frame can result in a bad
+   * state
+   *  @return true iff the last frame intersects with bad
+   *  post-condition: if true is returned, bad cube added to proof goals
+   */
+  bool intersects_bad();
+
+  /** Perform a IC3 step
+   *  @param i
+   */
+  ProverResult step(int i);
+
+  /** Perform the base IC3 step (zero case)
+   */
+  ProverResult step_0();
+
+  // Helper methods
+
+  /** Attempt to block all proof goals
+   *  to ensure termination, always choose proof goal with
+   *  smallest time
+   *  @return true iff all proof goals were blocked
+   */
+  bool block_all();
+
+  /** Attempt to block cube c at frame i
+   *  @param i the frame number
+   *  @param c the cube to try blocking
+   *  @return true iff the cube was blocked, otherwise a new proof goal was
+   * added to the proof goals
+   */
+  bool block(const IC3Goal & pg);
+
+  /** Try propagating all clauses from frame index i to the next frame.
+   *  @param i the frame index to propagate
+   *  @return true iff all the clauses are propagated (this means property was
+   * proven)
+   */
+  bool propagate(size_t i);
+
+  /** Add a new frame */
+  void push_frame();
+
+  /** Adds a constraint to frame i and (implicitly) all frames below it
+   *  @param i highest frame to add constraint to
+   *  @param constraint the constraint to add
+   */
+  void constrain_frame(size_t i, const IC3Unit & constraint);
+
+  /** Add all the terms at Frame i
+   *  Note: the frames_ data structure keeps terms only in the
+   *  highest frame where they are known to hold
+   *  Thus, asserting Fi actually needs to add terms
+   *  from Fi and all frames after it
+   *  @param i the frame number
+   */
+  void assert_frame_labels(size_t i) const;
+
+  smt::Term get_frame(size_t i) const;
+
+  void assert_trans_label() const;
+
+  /** Check if there are more proof goals
+   *  @return true iff there are more proof goals
+   */
+  bool has_proof_goals() const;
+
+  /** Gets a new proof goal (and removes it from proof_goals_)
+   *  @requires has_proof_goals()
+   *  @return a proof goal with the lowest available frame number
+   *  @alters proof_goals_
+   *  @ensures returned proof goal is from lowest frame in proof goals
+   */
+  IC3Goal get_next_proof_goal();
+
+  /** Create and add a proof goal for cube c for frame i
+   *  @param c the cube of the proof goal
+   *  @param i the frame number for the proof goal
+   *  @param n pointer to the proof goal that led to this one -- null for bad
+   *  (i.e. end of trace)
+   */
+  void add_proof_goal(const IC3Unit & c, size_t i, std::unique_ptr<IC3Goal> n);
+
+  /** Check if there are common assignments
+   *  between A and B
+   *  i.e. if A /\ B is SAT
+   *  @param A the first term
+   *  @param B the second term
+   *  @return true iff there is an intersection
+   */
+  bool intersects(const smt::Term & A, const smt::Term & B);
+
+  /** Check if the term intersects with the initial states
+   *  syntactic sugar for intersects(ts_.init(), t);
+   *  @param t the term to check
+   *  @return true iff t intersects with the initial states
+   */
+  bool intersects_initial(const smt::Term & t);
+
+  void fix_if_intersects_initial(smt::TermVec & to_keep,
+                                 const smt::TermVec & rem);
+
+  /** Returns the highest frame this unit can be pushed to
+   *  @param i the starting frame index
+   *  @param u the unit to check how far it can be pushed
+   *  @return index >= i such that this unit can be added
+   *          to that frame
+   */
+  size_t find_highest_frame(size_t i, const IC3Unit & u);
+
+  /** Pushes a solver context and keeps track of the context level
+   *  updates solver_context_
+   */
+  void push_solver_context();
+
+  /** Pops a solver context and keeps track of the context level
+   *  updates solver_context_
+   */
+  void pop_solver_context();
 };
 
 }  // namespace pono
