13a3c5e9fb23e8c1f34b8128ca12d72ffe7952ce
Hongce Zhang <zhanghongce@126.com>
2020-12-09 23:29:47 -0500

    partial model gen

---
+++ utils/sygus_ic3formula_helper.cpp
@@ -0,0 +1,44 @@
+/*********************                                                        */
+/*! \file syguspdr_ic3formula_helper.cpp
+** \verbatim
+** Top contributors (to current version):
+**   Hongce Zhang
+** This file is part of the pono project.
+** Copyright (c) 2020 by the authors listed in the file AUTHORS
+** in the top-level source directory) and their institutional affiliations.
+** All rights reserved.  See the file LICENSE in the top-level source
+** directory for licensing information.\endverbatim
+**
+** \brief Utility functions to get variables from ic3formula
+**        and to print it
+**
+**/
+
+#include "utils/sygus_ic3formula_helper.h"
+
+
+namespace pono {
+namespace sygus {
+
+void GetVariablesFromIC3Formula(const IC3Formula & f, smt::UnorderedTermSet & out) {
+  smt::get_free_symbols(f.term, out);
+}
+
+std::string PrintModel(const IC3Formula & f) {
+  std::string ret;
+  for (const auto & c : f.children) {
+    auto str = c->to_string();
+    auto eqidx = str.find("= ");
+    if (eqidx != std::string::npos) {
+      str.replace(eqidx, 2, ""); // remove "= ", o
+    }
+    if (ret.length() == 0)
+      ret = str;
+    else
+      ret = ret + " , " + str;
+  }
+  return ret;
+}
+
+}  // namespace sygus
+}  // namespace pono
