c45a52aeb2070822e6e3d145fda15376ad92cf57
makaimann <makaim@stanford.edu>
2020-12-29 15:05:04 -0800

    Temporarily pull a branch

--- contrib/setup-smt-switch.sh
+++ contrib/setup-smt-switch.sh
@@ -3,7 +3,8 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 DEPS=$DIR/../deps
 
-SMT_SWITCH_VERSION=2e6f51816852b605e06205c37664af882e5cc732
+# TEMP pull branch
+# SMT_SWITCH_VERSION=2e6f51816852b605e06205c37664af882e5cc732
 
 usage () {
     cat <<EOF
@@ -59,9 +60,10 @@ mkdir -p $DEPS
 
 if [ ! -d "$DEPS/smt-switch" ]; then
     cd $DEPS
-    git clone https://github.com/makaimann/smt-switch
+    git clone -b get-id https://github.com/makaimann/smt-switch
     cd smt-switch
-    git checkout -f $SMT_SWITCH_VERSION
+    # TEMP pull branch
+    # git checkout -f $SMT_SWITCH_VERSION
     ./contrib/setup-btor.sh
     if [ $cvc4_home = default ]; then
         ./contrib/setup-cvc4.sh
