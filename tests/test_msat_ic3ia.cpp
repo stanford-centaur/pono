e312c555cf76866e699b747ec9cea9006a968fb9
makaimann <makaim@stanford.edu>
2021-01-04 21:14:58 -0800

    Fixes for msat-ic3ia after prover-update

--- tests/test_msat_ic3ia.cpp
+++ tests/test_msat_ic3ia.cpp
@@ -37,9 +37,9 @@ TEST_P(MsatIC3IAUnitTests, IntCounterSafe)
   rts.constrain_init(rts.make_term(Equal, c, rts.make_term(0, intsort)));
   rts.assign_next(c, rts.make_term(Plus, c, rts.make_term(1, intsort)));
 
-  Property p(rts, rts.make_term(Ge, c, rts.make_term(0, intsort)));
+  Property p(s, rts.make_term(Ge, c, rts.make_term(0, intsort)));
 
-  MsatIC3IA msat_ic3ia(p, s);
+  MsatIC3IA msat_ic3ia(p, rts, s);
   ProverResult res = msat_ic3ia.prove();
   EXPECT_EQ(res, ProverResult::TRUE);
 
@@ -61,9 +61,9 @@ TEST_P(MsatIC3IAUnitTests, IntCounterUnsafe)
                     rts.next(c),
                     rts.make_term(Plus, c, rts.make_term(1, intsort)))));
 
-  Property p(rts, rts.make_term(Ge, c, rts.make_term(0, intsort)));
+  Property p(s, rts.make_term(Ge, c, rts.make_term(0, intsort)));
 
-  MsatIC3IA msat_ic3ia(p, s);
+  MsatIC3IA msat_ic3ia(p, rts, s);
   ProverResult res = msat_ic3ia.prove();
   EXPECT_EQ(res, ProverResult::FALSE);
 
