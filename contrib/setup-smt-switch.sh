bab3f2c684889aeac39661f407392f7161402b03
makaimann <makaim@stanford.edu>
2021-01-12 11:30:41 -0800

    Point back to smt-switch master branch

--- contrib/setup-smt-switch.sh
+++ contrib/setup-smt-switch.sh
@@ -3,8 +3,7 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 DEPS=$DIR/../deps
 
-# TEMP pull branch
-# SMT_SWITCH_VERSION=9230477a5b9cd287cf4a30ed53d20700f714f821
+SMT_SWITCH_VERSION=0c0b12a11b9a0d68d6d724389393120172ed50f3
 
 usage () {
     cat <<EOF
@@ -60,10 +59,9 @@ mkdir -p $DEPS
 
 if [ ! -d "$DEPS/smt-switch" ]; then
     cd $DEPS
-    git clone -b update-assuming-interface https://github.com/makaimann/smt-switch
+    git clone https://github.com/makaimann/smt-switch
     cd smt-switch
-    # TEMP pull branch
-    # git checkout -f $SMT_SWITCH_VERSION
+    git checkout -f $SMT_SWITCH_VERSION
     ./contrib/setup-btor.sh
     if [ $cvc4_home = default ]; then
         ./contrib/setup-cvc4.sh
