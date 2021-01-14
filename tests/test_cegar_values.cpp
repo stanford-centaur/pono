8c4a630279393b4606a2580634ad6572236fc1fe
makaimann <makaim@stanford.edu>
2021-01-14 11:54:21 -0800

    Add a simple unsafe test

--- tests/test_cegar_values.cpp
+++ tests/test_cegar_values.cpp
@@ -15,7 +15,7 @@ using namespace std;
 
 namespace pono_tests {
 
-TEST(CegValues, Simple)
+TEST(CegValues, SimpleSafe)
 {
   set_global_logger_verbosity(1);
 
@@ -53,6 +53,43 @@ TEST(CegValues, Simple)
   cout << "got invariant " << invar << endl;
   // can't check invariant because it has prophecy variables in it
   // TODO consider checking the universally quantified version with CVC4
+}
+
+TEST(CegValues, SimpleUnsafe)
+{
+  set_global_logger_verbosity(1);
+
+  SmtSolver s = create_solver(MSAT);
+  RelationalTransitionSystem rts(s);
+  Sort bvsort8 = rts.make_sort(BV, 8);
+  Sort bvsort32 = rts.make_sort(BV, 32);
+  Sort arrsort = rts.make_sort(ARRAY, bvsort8, bvsort32);
+  Term i = rts.make_statevar("i", bvsort8);
+  Term j = rts.make_statevar("j", bvsort8);
+  Term d = rts.make_statevar("d", bvsort32);
+  Term a = rts.make_statevar("a", arrsort);
+
+  Term constarr0 = rts.make_term(rts.make_term(0, bvsort32), arrsort);
+  rts.set_init(rts.make_term(Equal, a, constarr0));
+  rts.assign_next(
+      a,
+      rts.make_term(Ite,
+                    // off by one in the update
+                    // e.g. using <= instead of <
+                    rts.make_term(BVUle, d, rts.make_term(200, bvsort32)),
+                    rts.make_term(Store, a, i, d),
+                    a));
+
+  Term prop_term = rts.make_term(
+      BVUlt, rts.make_term(Select, a, j), rts.make_term(200, bvsort32));
+  Property prop(s, prop_term);
+
+  // TODO create a make_ command for this
+  shared_ptr<Prover> ceg =
+      make_shared<CegarValues<CegProphecyArrays<IC3IA>>>(prop, rts, s);
+
+  ProverResult r = ceg->check_until(5);
+  ASSERT_EQ(r, ProverResult::FALSE);
 }
 
 }  // namespace pono_tests
