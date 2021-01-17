43224a3d7b5dbfcdd7816a21406094366fb7cdab
Hongce Zhang <zhanghongce@126.com>
2021-01-17 14:14:55 -0500

    adapt for base ic3 changes

--- engines/syguspdr.cpp
+++ engines/syguspdr.cpp
@@ -1126,7 +1126,7 @@ ProverResult SygusPdr::check_until(int k)
   RefineResult ref_res;
   int i = reached_k_ + 1;
   assert(reached_k_ + 1 >= 0);
-  for (size_t i = reached_k_ + 1; i <= k; ++i) {
+  while (i <= k) {
     res = step(i);
 
     if (res == ProverResult::FALSE) {
@@ -1142,6 +1142,8 @@ ProverResult SygusPdr::check_until(int k)
         assert(s == REFINE_FAIL);
         throw PonoException("Refinement failed");
       }
+    } else {
+      ++i;
     }
 
     if (res != ProverResult::UNKNOWN) {
