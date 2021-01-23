b9d5644effddf0bb6e48dd604feb6c1b5f3521c5
makaimann <makaim@stanford.edu>
2021-01-22 19:05:23 -0800

    Expose ic3bits to the command line

--- options/options.h
+++ options/options.h
@@ -48,6 +48,7 @@ const std::unordered_map<std::string, Engine> str2engine(
       { "ind", KIND },
       { "interp", INTERP },
       { "mbic3", MBIC3 },
+      { "ic3bits", IC3_BITS },
       { "ic3ia", IC3IA_ENGINE },
       { "msat-ic3ia", MSAT_IC3IA } });
 
