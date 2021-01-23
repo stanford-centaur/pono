4cbd5a714f70fe5854765ee008fdd79427383066
makaimann <makaim@stanford.edu>
2021-01-22 19:02:41 -0800

    Add simple safe test

--- tests/test_ic3bits.cpp
+++ tests/test_ic3bits.cpp
@@ -6,6 +6,7 @@
 #include "gtest/gtest.h"
 #include "smt/available_solvers.h"
 #include "tests/common_ts.h"
+#include "utils/ts_analysis.h"
 
 using namespace pono;
 using namespace smt;
@@ -34,12 +35,31 @@ TEST_P(IC3BitsUnitTests, CounterSystemUnsafe)
   counter_system(fts, max_val);
   Term x = fts.named_terms().at("x");
 
+  // off-by-one in property -- unsafe
   Term prop_term = s->make_term(BVUlt, x, max_val);
   Property p(s, prop_term);
 
   IC3Bits ic3bits(p, fts, s);
   ProverResult r = ic3bits.check_until(12);
   ASSERT_EQ(r, FALSE);
+}
+
+TEST_P(IC3BitsUnitTests, CounterSystemSafe)
+{
+  FunctionalTransitionSystem fts(s);
+  Term max_val = fts.make_term(10, bvsort8);
+  counter_system(fts, max_val);
+  Term x = fts.named_terms().at("x");
+
+  // safe property
+  Term prop_term = s->make_term(BVUle, x, max_val);
+  Property p(s, prop_term);
+
+  IC3Bits ic3bits(p, fts, s);
+  ProverResult r = ic3bits.prove();
+  ASSERT_EQ(r, TRUE);
+  Term invar = ic3bits.invar();
+  ASSERT_TRUE(check_invar(fts, prop_term, invar));
 }
 
 INSTANTIATE_TEST_SUITE_P(ParameterizedSolverIC3BitsUnitTests,
