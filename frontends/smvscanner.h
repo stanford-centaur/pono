50bba0eac33c36b499b2385fcf6a3d6e6a82d7a5
Makai Mann <makaim@stanford.edu>
2021-04-14 20:56:50 -0400

    Add debug options

--- frontends/smvscanner.h
+++ frontends/smvscanner.h
@@ -14,7 +14,11 @@ class SMVEncoder;
 class SMVscanner : public yyFlexLexer
 {
  public:
-  SMVscanner(SMVEncoder & encoder) : _encoder(encoder) {}
+  SMVscanner(SMVEncoder & encoder) : _encoder(encoder)
+  {
+    // Uncomment this and debug option in smvparser.l for output
+    // yy_flex_debug = 1;
+  }
   virtual ~SMVscanner() {}
   virtual pono::smvparser::symbol_type yylex(SMVEncoder & encoder);
 
