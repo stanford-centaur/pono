f8ca28501aff9f66edf505054952302ad602fb67
makaimann <makaim@stanford.edu>
2021-08-16 09:42:35 -0400

    Don't error on systems without arrays

--- engines/ceg_prophecy_arrays.cpp
+++ engines/ceg_prophecy_arrays.cpp
@@ -256,7 +256,7 @@ void CegProphecyArrays<Prover_T>::initialize()
   }
 
   if (!contains_arrays) {
-    logger.log(1, "Warning: running on counterexample-guided prophecy for arrays on system without arrays.")
+    logger.log(1, "Warning: running on counterexample-guided prophecy for arrays on system without arrays.");
   }
 }
 
