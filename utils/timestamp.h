b7b02b42dfb32e917638bcec587d0c3925b94769
lonsing <1989839+lonsing@users.noreply.github.com>
2021-11-05 16:07:40 -0700

    added header stub

---
+++ utils/timestamp.h
@@ -0,0 +1,30 @@
+/*********************                                                        */
+/*! \file
+ ** \verbatim
+ ** Top contributors (to current version):
+ **   Florian Lonsing
+ ** This file is part of the pono project.
+ ** Copyright (c) 2021 by the authors listed in the file AUTHORS
+ ** in the top-level source directory) and their institutional affiliations.
+ ** All rights reserved.  See the file LICENSE in the top-level source
+ ** directory for licensing information.\endverbatim
+ **
+ ** \brief
+ **
+ **
+ **/
+
+#pragma once
+
+#include <iostream>
+#include <chrono>
+
+
+/*************************************** time stamp functions
+ * ************************************************/
+
+namespace pono {
+
+  
+
+}  // namespace pono
