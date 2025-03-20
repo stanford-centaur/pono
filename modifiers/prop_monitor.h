e389393d8611930fd90a8667fcce2467a6d23248
Áron Ricardo Perez-Lopez <arpl@cs.stanford.edu>
2025-03-20 00:11:44 -0700

    Move add_prop_monitor implementation out of header

--- modifiers/prop_monitor.h
+++ modifiers/prop_monitor.h
@@ -16,46 +16,11 @@
 
 #pragma once
 
-#include "core/rts.h"
 #include "core/ts.h"
-#include "utils/logger.h"
+#include "smt-switch/smt.h"
 
 namespace pono {
 
-smt::Term add_prop_monitor(TransitionSystem & ts, const smt::Term & prop)
-{
-  logger.log(1, "Adding a monitor for the property");
-
-  smt::Term monitor;
-  size_t id = 0;
-  while (true) {
-    try {
-      monitor = ts.make_statevar("_monitor_" + std::to_string(id),
-                                 ts.make_sort(smt::BOOL));
-      break;
-    }
-    catch (SmtException & e) {
-      ++id;
-    }
-  }
-  assert(monitor);
-
-  // monitor starts true
-  ts.constrain_init(monitor);
-
-  if (ts.no_next(prop)) {
-    ts.assign_next(monitor, prop);
-  } else if (!ts.is_functional()) {
-    RelationalTransitionSystem & rts =
-      static_cast<RelationalTransitionSystem &>(ts);
-    rts.constrain_trans(rts.make_term(smt::Equal, rts.next(monitor), prop));
-  } else {
-    assert(ts.is_functional());
-    throw PonoException(
-        "Cannot use next in property of a functional transition system.");
-  }
-
-  return monitor;
-}
+smt::Term add_prop_monitor(TransitionSystem & ts, const smt::Term & prop);
 
 }  // namespace pono
