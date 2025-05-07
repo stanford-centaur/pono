8132cb831a2f47cb02474f4a1aa58105064ae442
Áron Ricardo Perez-Lopez <arpl@cs.stanford.edu>
2025-05-08 01:41:37 +0200

    Update smt-switch to 2e726ab

--- contrib/setup-smt-switch.sh
+++ contrib/setup-smt-switch.sh
@@ -4,7 +4,7 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 DEPS=$DIR/../deps
 
-SMT_SWITCH_VERSION=1f708c2da4aa102e45848b6a349d1d3260d262a5
+SMT_SWITCH_VERSION=2e726abf28250d9febf67175a23ad9397664361b
 
 usage () {
     cat <<EOF
