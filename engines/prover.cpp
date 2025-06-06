c9fcae468d2eff2be7e57388bef1c5599aa45c6b
Po-Chun Chien <po-chun.chien@sosy.ifi.lmu.de>
2025-06-05 17:17:30 -0700

    Create a SafetyProver subclass from Prover

--- engines/prover.cpp
+++ engines/prover.cpp
@@ -41,125 +41,15 @@ Prover::Prover(const Property & p,
       orig_ts_(ts),
       ts_(ts, to_prover_solver_),
       unroller_(ts_),
-      bad_(solver_->make_term(smt::PrimOp::Not,
-                              ts_.solver() == orig_property_.solver()
-                                  ? orig_property_.prop_term()
-                                  : to_prover_solver_.transfer_term(
-                                        orig_property_.prop_term(), BOOL))),
-      options_(opt),
-      engine_(Engine::NONE)
+      options_(opt)
 {
 }
 
 Prover::~Prover() {}
 
-void Prover::initialize()
-{
-  if (initialized_) {
-    return;
-  }
-
-  reached_k_ = -1;
-
-  if (!ts_.only_curr(bad_)) {
-    throw PonoException(
-        "Property should not contain inputs or next state variables");
-  }
-
-  initialized_ = true;
-}
-
 ProverResult Prover::prove() { return check_until(INT_MAX); }
 
-bool Prover::witness(std::vector<UnorderedTermMap> & out)
-{
-  if (!witness_.size()) {
-    throw PonoException(
-        "Recovering witness failed. Make sure that there was "
-        "a counterexample and that the engine supports witness generation.");
-  }
-
-  function<Term(const Term &, SortKind)> transfer_to_prover_as;
-  function<Term(const Term &, SortKind)> transfer_to_orig_ts_as;
-  TermTranslator to_orig_ts_solver(orig_ts_.solver());
-  if (solver_ == orig_ts_.solver()) {
-    // don't need to transfer terms if the solvers are the same
-    transfer_to_prover_as = [](const Term & t, SortKind sk) { return t; };
-    transfer_to_orig_ts_as = [](const Term & t, SortKind sk) { return t; };
-  } else {
-    // need to add symbols to cache
-    UnorderedTermMap & cache = to_orig_ts_solver.get_cache();
-    for (const auto & v : orig_ts_.statevars()) {
-      cache[to_prover_solver_.transfer_term(v)] = v;
-    }
-    for (const auto & v : orig_ts_.inputvars()) {
-      cache[to_prover_solver_.transfer_term(v)] = v;
-    }
-
-    transfer_to_prover_as = [this](const Term & t, SortKind sk) {
-      return to_prover_solver_.transfer_term(t, sk);
-    };
-    transfer_to_orig_ts_as = [&to_orig_ts_solver](const Term & t, SortKind sk) {
-      return to_orig_ts_solver.transfer_term(t, sk);
-    };
-  }
-
-  bool success = true;
-
-  // Some backends don't support full witnesses
-  // it will still populate state variables, but will return false instead of
-  // true
-  for (auto wit_map : witness_) {
-    out.push_back(UnorderedTermMap());
-    UnorderedTermMap & map = out.back();
-
-    for (const auto & v : orig_ts_.statevars()) {
-      const SortKind & sk = v->get_sort()->get_sort_kind();
-      const Term & pv = transfer_to_prover_as(v, sk);
-      map[v] = transfer_to_orig_ts_as(wit_map.at(pv), sk);
-    }
-
-    for (const auto & v : orig_ts_.inputvars()) {
-      const SortKind & sk = v->get_sort()->get_sort_kind();
-      const Term & pv = transfer_to_prover_as(v, sk);
-      try {
-        map[v] = transfer_to_orig_ts_as(wit_map.at(pv), sk);
-      }
-      catch (std::exception & e) {
-        success = false;
-        break;
-      }
-    }
-
-    if (success) {
-      for (const auto & elem : orig_ts_.named_terms()) {
-        const SortKind & sk = elem.second->get_sort()->get_sort_kind();
-        const Term & pt = transfer_to_prover_as(elem.second, sk);
-        try {
-          map[elem.second] = transfer_to_orig_ts_as(wit_map.at(pt), sk);
-        }
-        catch (std::exception & e) {
-          success = false;
-          break;
-        }
-      }
-    }
-  }
-
-  return success;
-}
-
 size_t Prover::witness_length() const { return reached_k_ + 1; }
-
-Term Prover::invar()
-{
-  if (!invar_) {
-    throw PonoException(
-        "Failed to return invar. Be sure that the property was proven by an "
-        "engine the supports returning invariants.");
-  }
-  return to_orig_ts(invar_, BOOL);
-}
 
 Term Prover::to_orig_ts(Term t, SortKind sk)
 {
@@ -186,35 +76,6 @@ Term Prover::to_orig_ts(Term t, SortKind sk)
 Term Prover::to_orig_ts(Term t)
 {
   return to_orig_ts(t, t->get_sort()->get_sort_kind());
-}
-
-bool Prover::compute_witness()
-{
-  // TODO: make sure the solver state is SAT
-
-  for (int i = 0; i <= reached_k_ + 1; ++i) {
-    witness_.push_back(UnorderedTermMap());
-    UnorderedTermMap & map = witness_.back();
-
-    for (const auto & v : ts_.statevars()) {
-      const Term & vi = unroller_.at_time(v, i);
-      const Term & r = solver_->get_value(vi);
-      map[v] = r;
-    }
-
-    for (const auto & v : ts_.inputvars()) {
-      const Term & vi = unroller_.at_time(v, i);
-      const Term & r = solver_->get_value(vi);
-      map[v] = r;
-    }
-
-    for (const auto & elem : ts_.named_terms()) {
-      const Term & ti = unroller_.at_time(elem.second, i);
-      map[elem.second] = solver_->get_value(ti);
-    }
-  }
-
-  return true;
 }
 
 }  // namespace pono
