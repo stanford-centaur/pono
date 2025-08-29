c846a824ad15ff17ce5af6f5a30e7cbb3acad498
Áron Ricardo Perez-Lopez <arpl@cs.stanford.edu>
2025-08-28 19:09:56 -0700

    k-induction: fix error when solver returns unknown (#451)
    * Handle all non-SAT results
    
    Co-authored-by: Po-Chun Chien <po-chun.chien@sosy.ifi.lmu.de>

--- engines/kinduction.cpp
+++ engines/kinduction.cpp
@@ -360,6 +360,11 @@ bool KInduction::check_simple_path_lazy(int i)
     // we return immediately here and skip simple path checking.
     if (options_.kind_no_simple_path_check_) return false;
 
+    if (!r.is_sat()) {
+      kind_log_msg(2, "      ", "simple path check {}", r.to_string());
+      return false;
+    }
+
     added_to_simple_path = false;
 
     for (int j = 0; j < i && (no_multi_call || !added_to_simple_path); ++j) {
