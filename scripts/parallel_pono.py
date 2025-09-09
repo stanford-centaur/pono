2c03c4d00aa18608e56e70d800fc39ac9f35a2e7
Áron Ricardo Perez-Lopez <arpl@cs.stanford.edu>
2025-09-08 23:21:25 -0700

    Use `shutil.move` instead of `pathlib.Path.rename` to create witness
    `pathlib.Path.rename` and `os.rename` may fail across filesystems.

--- scripts/parallel_pono.py
+++ scripts/parallel_pono.py
@@ -181,7 +181,7 @@ def main() -> int:
                 else:
                     print(process.stdout.read())
                 if args.witness_file:
-                    witnesses[name].rename(args.witness_file)
+                    shutil.move(witnesses[name], args.witness_file)
                 return process.returncode
 
     return ReturnCode.UNKNOWN.value
