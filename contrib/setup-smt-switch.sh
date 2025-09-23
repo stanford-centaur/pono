0c3014593d78c81c3a297cfb18d99d05639d91bf
Áron Ricardo Perez-Lopez <arpl@cs.stanford.edu>
2025-09-23 15:38:08 -0700

    Update smt-switch 22c1066
    Updates:
    * Allow custom download step in contrib scripts by @CyanoKobalamyne in https://github.com/stanford-centaur/smt-switch/pull/422
    * Install tracer.hpp header for cadical by @CyanoKobalamyne in https://github.com/stanford-centaur/smt-switch/pull/423
    * Allow extra options for `meson compile` by @CyanoKobalamyne in https://github.com/stanford-centaur/smt-switch/pull/424
    * Update Boolector to last version by @CyanoKobalamyne in https://github.com/stanford-centaur/smt-switch/pull/425

--- contrib/setup-smt-switch.sh
+++ contrib/setup-smt-switch.sh
@@ -4,7 +4,7 @@ set -e
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 DEPS=$DIR/../deps
 
-SMT_SWITCH_VERSION=ac27d8c7d13b7dd6a84211047cc9d1355437d8a1
+SMT_SWITCH_VERSION=22c1066d0e3a41cc5323188cb714735e14e9fad3
 
 usage () {
     cat <<EOF
