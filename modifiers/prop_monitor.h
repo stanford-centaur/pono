2eece73011cb019b42ffad2c02ff2acc3fe8db7b
Po-Chun Chien <po-chun.chien@sosy.ifi.lmu.de>
2025-09-29 18:26:55 +0200

    When adding monitor, ensure there will always be a next state if property is violated

--- modifiers/prop_monitor.h
+++ modifiers/prop_monitor.h
@@ -22,9 +22,33 @@
 
 namespace pono {
 
+/** Adds a monitor state variable to the transition system for the given
+ *  property. The monitor variable is initialized to true and holds the value of
+ * `prop` at the previous time step. The transition system is modified in place.
+ *
+ *  The function can only be used when
+ *  (1) `prop` contains a next-state variable (and `ts` is relational) or
+ *  (2) the transition relation `ts.trans()` is right-total.
+ *  This is to ensure that `ts` allows at least one further transition when
+ * `prop` is falsified (recall that the monitor has a one-step delay).
+ *
+ *  NOTE: only works for safety properties currently.
+ *
+ *  @param ts the transition system to modify
+ *  @param prop the property term to monitor
+ *  @return the monitor state variable term
+ */
 smt::Term add_prop_monitor(TransitionSystem & ts, const smt::Term & prop)
 {
+  // only in debug mode as right-total check can be expensive
+  assert(!ts.no_next(prop) || ts.is_right_total());
   logger.log(1, "Adding a monitor for the property");
+
+  // checks for functional TS
+  if (ts.is_functional() && !ts.no_next(prop)) {
+    throw PonoException(
+        "Cannot use next in property of a functional transition system.");
+  }
 
   smt::Term monitor;
   size_t id = 0;
@@ -45,14 +69,11 @@ smt::Term add_prop_monitor(TransitionSystem & ts, const smt::Term & prop)
 
   if (ts.no_next(prop)) {
     ts.assign_next(monitor, prop);
-  } else if (!ts.is_functional()) {
+  } else {
+    assert(!ts.is_functional());
     RelationalTransitionSystem & rts =
         static_cast<RelationalTransitionSystem &>(ts);
     rts.constrain_trans(rts.make_term(smt::Equal, rts.next(monitor), prop));
-  } else {
-    assert(ts.is_functional());
-    throw PonoException(
-        "Cannot use next in property of a functional transition system.");
   }
 
   return monitor;
