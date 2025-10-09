4b83f839c22751b0fbc319610d21cc7091afb612
Po-Chun Chien <po-chun.chien@sosy.ifi.lmu.de>
2025-10-09 02:09:49 +0200

    Soundly add monitor state (#470)
    * When adding monitor, ensure there will always be a next state if property is violated
    
    * Split into .h and .cpp
    
    * Avoid running same checks multiple times
    
    * Update doc

--- modifiers/prop_monitor.h
+++ modifiers/prop_monitor.h
@@ -2,7 +2,7 @@
 /*! \file prop_monitor.h
 ** \verbatim
 ** Top contributors (to current version):
-**   Makai Mann, Ahmed Irfan
+**   Makai Mann, Ahmed Irfan, Po-Chun Chien
 ** This file is part of the pono project.
 ** Copyright (c) 2019 by the authors listed in the file AUTHORS
 ** in the top-level source directory) and their institutional affiliations.
@@ -16,46 +16,26 @@
 
 #pragma once
 
-#include "core/rts.h"
 #include "core/ts.h"
-#include "utils/logger.h"
 
 namespace pono {
 
-smt::Term add_prop_monitor(TransitionSystem & ts, const smt::Term & prop)
-{
-  logger.log(1, "Adding a monitor for the property");
-
-  smt::Term monitor;
-  size_t id = 0;
-  while (true) {
-    try {
-      monitor = ts.make_statevar("_monitor_" + std::to_string(id),
-                                 ts.make_sort(smt::BOOL));
-      break;
-    }
-    catch (SmtException & e) {
-      ++id;
-    }
-  }
-  assert(monitor);
-
-  // monitor starts true
-  ts.constrain_init(monitor);
-
-  if (ts.no_next(prop)) {
-    ts.assign_next(monitor, prop);
-  } else if (!ts.is_functional()) {
-    RelationalTransitionSystem & rts =
-        static_cast<RelationalTransitionSystem &>(ts);
-    rts.constrain_trans(rts.make_term(smt::Equal, rts.next(monitor), prop));
-  } else {
-    assert(ts.is_functional());
-    throw PonoException(
-        "Cannot use next in property of a functional transition system.");
-  }
-
-  return monitor;
-}
+/** Adds a monitor state variable to the transition system for the given
+ *  property. The monitor variable is initialized to true and holds the value of
+ * `prop` at the previous time step. The transition system is modified in place.
+ *
+ *  The function can only be used when
+ *  (1) `prop` contains a next-state variable (and `ts` is relational) or
+ *  (2) the transition relation `ts.trans()` is right-total.
+ *  This is to ensure that `ts` allows at least one further transition when
+ * `prop` is falsified (recall that the monitor has a one-step delay).
+ *
+ *  NOTE: only works for safety properties currently.
+ *
+ *  @param ts the transition system to modify
+ *  @param prop the property term to monitor
+ *  @return the monitor state variable term
+ */
+smt::Term add_prop_monitor(TransitionSystem & ts, const smt::Term & prop);
 
 }  // namespace pono
