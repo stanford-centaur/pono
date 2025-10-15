7714980727c312b3e61e5209f5cf53414f61fec2
Áron Ricardo Perez-Lopez <arpl@cs.stanford.edu>
2025-10-15 01:58:45 -0700

    Add GitHub CI script to build and test binary

--- contrib/setup-smt-switch.sh
+++ contrib/setup-smt-switch.sh
@@ -59,7 +59,7 @@ mkdir -p $DEPS
 
 if [ ! -d "$DEPS/smt-switch" ]; then
     cd $DEPS
-    git clone https://github.com/makaimann/smt-switch
+    git clone https://github.com/stanford-centaur/smt-switch
     cd smt-switch
     git checkout -f $SMT_SWITCH_VERSION
     ./contrib/setup-btor.sh
