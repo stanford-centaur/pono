f142518cbc6706fa931c4f028dd8fca3402a4f1c
Áron Ricardo Perez-Lopez <arpl@cs.stanford.edu>
2026-07-08 16:26:02 -0700

    Format shell scripts with shfmt

--- contrib/setup-ic3ia.sh
+++ contrib/setup-ic3ia.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 DEPS=$DIR/../deps
 
-usage () {
-cat <<EOF
+usage() {
+  cat <<EOF
 Usage: $0 [<option> ...]
 
 Configures the CMAKE build environment.
@@ -16,9 +16,9 @@ EOF
   exit 0
 }
 
-die () {
-    echo "*** $0: $*" 1>&2
-    exit 1
+die() {
+  echo "*** $0: $*" 1>&2
+  exit 1
 }
 
 mkdir -p "$DEPS"
@@ -27,42 +27,41 @@ cd "$DEPS"
 msat_home="$DEPS/mathsat"
 ic3ia_version=ic3ia-23.05
 
-while [[ $# -gt 0 ]]
-do
-    case $1 in
-        -h|--help) usage;;
-        --msat-home=*)
-            msat_home=${1##*=}
-            # Check if msat_home is an absolute path and if not, make it
-            # absolute.
-            case $msat_home in
-                /*) ;;                            # absolute path
-                *) msat_home=$(pwd)/$msat_home ;; # make absolute path
-            esac
-            ;;
-        *) die "unexpected argument: $1";;
-    esac
-    shift
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    -h | --help) usage ;;
+    --msat-home=*)
+      msat_home=${1##*=}
+      # Check if msat_home is an absolute path and if not, make it
+      # absolute.
+      case $msat_home in
+        /*) ;;                            # absolute path
+        *) msat_home=$(pwd)/$msat_home ;; # make absolute path
+      esac
+      ;;
+    *) die "unexpected argument: $1" ;;
+  esac
+  shift
 done
 
 if [[ -d "$DEPS/ic3ia" ]]; then
-    echo "It appears ic3ia was already downloaded"
-    echo "If you'd like to rebuild, please manually delete it"
-    exit 1
+  echo "It appears ic3ia was already downloaded"
+  echo "If you'd like to rebuild, please manually delete it"
+  exit 1
 fi
 
-if [[ ! -d "$msat_home" ]]; then
-    echo "Could not locate mathsat"
-    echo "Required for building ic3ia"
-    echo "Looked here: $msat_home"
-    exit 1
+if [[ ! -d $msat_home ]]; then
+  echo "Could not locate mathsat"
+  echo "Required for building ic3ia"
+  echo "Looked here: $msat_home"
+  exit 1
 fi
 
 curl -Lk https://es-static.fbk.eu/people/griggio/ic3ia/$ic3ia_version.tar.gz --output "$DEPS/$ic3ia_version.tar.gz"
 
 if [[ ! -f "$DEPS/$ic3ia_version.tar.gz" ]]; then
-    echo "It appears that downloading ic3ia failed."
-    exit 1
+  echo "It appears that downloading ic3ia failed."
+  exit 1
 fi
 
 tar -xf $ic3ia_version.tar.gz
