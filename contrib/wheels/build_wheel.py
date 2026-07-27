9364503871016aab8f7489f74fb4aaa61d967ae2
Áron Ricardo Perez-Lopez <arpl@cs.stanford.edu>
2026-07-26 21:21:50 -0400

    Remove "All rights reserved." from Python copyright headers (#579)
    The headers added in #578 included this sentence unlike the project's
    intended standard wording; drop it and reflow the surrounding lines.
    
    See
    https://liferay.dev/b/how-and-why-to-properly-write-copyright-statements-in-your-code#what-is-wrong-with-all-rights-reserved
    for why "All rights reserved." is unnecessary and should be omitted.

--- contrib/wheels/build_wheel.py
+++ contrib/wheels/build_wheel.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 # Copyright (c) 2020 by the authors listed in the file AUTHORS in the top-level
-# source directory and their institutional affiliations. All rights reserved.
-# See the file LICENSE in the top-level source directory for licensing
-# information.
+# source directory and their institutional affiliations. See the file LICENSE
+# in the top-level source directory for licensing information.
 #
 # This file is part of the pono project.
 import multiprocessing
